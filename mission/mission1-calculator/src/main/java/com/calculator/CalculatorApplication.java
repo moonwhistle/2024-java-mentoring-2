2fab9adbbf3a9682672d06b79e2a18c1a00d68e8
pooreunblue <mjbsh02@mju.ac.kr>
2024-09-23 18:57:20 +0900

    Feat: step1 계산기 생성 및 구현

--- mission/mission1-calculator/src/main/java/com/calculator/CalculatorApplication.java
+++ mission/mission1-calculator/src/main/java/com/calculator/CalculatorApplication.java
@@ -1,7 +1,14 @@
 package com.calculator;
 
+import com.calculator.controller.CalculatorController;
+import com.calculator.view.InputView;
+import com.calculator.view.OutputView;
+
 public class CalculatorApplication {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        CalculatorController calculatorController = new CalculatorController(inputView, outputView);
+        calculatorController.controllerStart();
     }
 }
--- mission/mission1-calculator/src/main/java/com/calculator/domain/Calculator.java
+++ mission/mission1-calculator/src/main/java/com/calculator/domain/Calculator.java
@@ -1,12 +1,21 @@
 package com.calculator.domain;
 
 public class Calculator {
-
-    public int add() {
-        return 0;
+    public int add(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
     }
 
-    public int minus() {
-        return 0;
+    public int subtract(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public int multiply(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    public int divide(int firstNumber, int secondNumber) {
+        if (secondNumber == 0) throw new ArithmeticException("Division by zero");
+        return firstNumber / secondNumber;
     }
 }
+

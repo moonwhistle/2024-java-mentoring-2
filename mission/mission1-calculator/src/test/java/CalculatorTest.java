edf91ed1edc55f2a0d5a9c0ddce7028abfbb742b
daeyeon <kdy4410121@naver.com>
2024-09-25 19:06:59 +0900

    feat: 나누기 실패 test 메서드 생성

--- mission/mission1-calculator/src/test/java/CalculatorTest.java
+++ mission/mission1-calculator/src/test/java/CalculatorTest.java
@@ -45,5 +45,17 @@ public class CalculatorTest {
 
         assertEquals(result, expected);
     }
+
+    @Test
+    @DisplayName("계산기 나누기 메서드 실패 테스트")
+    public void testDivideFail(){
+        Calculator calculator = new Calculator();
+        final int num1 = 3;
+        final int num2 = 0;
+
+        final var expected = "0으로 나눌 수 없음";
+        final ArithmeticException arithmeticException = assertThrows(ArithmeticException.class, ()-> calculator.divide(num1,num2));
+        assertEquals(arithmeticException.getMessage(), expected);
+    }
 }
 

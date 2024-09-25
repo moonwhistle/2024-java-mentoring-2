7ffb9c147a4325ed5220c0adb1f0186520c778b7
daeyeon <kdy4410121@naver.com>
2024-09-25 19:06:06 +0900

    feat: 더하기 test 메서드 생성

--- mission/mission1-calculator/src/test/java/CalculatorTest.java
+++ mission/mission1-calculator/src/test/java/CalculatorTest.java
@@ -1,2 +1,24 @@
+import com.calculator.domain.Calculator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
 public class CalculatorTest {
+    @Test
+    @DisplayName("계산기 더하기 메서드 테스트")
+    public void testAdd(){
+        Calculator calculator = new Calculator();
+        final int num1 = 3;
+        final int num2 = 4;
+
+        final int expected = 7;
+        final int result = calculator.add(num1, num2);
+
+        assertEquals(result, expected);
+    }
+    
 }
+

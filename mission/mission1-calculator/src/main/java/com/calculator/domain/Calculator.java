8a1c452751a61862f47867d6fdfbde7f8ad5b74f
daeyeon <kdy4410121@naver.com>
2024-09-25 18:52:59 +0900

    feat: 곱하기 메서드 생성

--- mission/mission1-calculator/src/main/java/com/calculator/domain/Calculator.java
+++ mission/mission1-calculator/src/main/java/com/calculator/domain/Calculator.java
@@ -8,4 +8,9 @@ public class Calculator {
     public int subtract(int num1, int num2){
         return num1 - num2;
     }
+
+    public int multiple(int num1 , int num2){
+        return num1 * num2;
+    }
+
 }

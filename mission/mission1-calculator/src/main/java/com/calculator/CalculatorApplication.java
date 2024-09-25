35f52f7afd1222b90ee3e5b19fc13db60a8712c5
daeyeon <kdy4410121@naver.com>
2024-09-25 19:27:33 +0900

    feat: main 메서드에서 계산 메서드 생성

--- mission/mission1-calculator/src/main/java/com/calculator/CalculatorApplication.java
+++ mission/mission1-calculator/src/main/java/com/calculator/CalculatorApplication.java
@@ -1,7 +1,15 @@
 package com.calculator;
 
+import com.calculator.controller.CalculatorController;
+import com.calculator.view.InputView;
+import com.calculator.view.OutputView;
+
 public class CalculatorApplication {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        CalculatorController Controller = new CalculatorController(inputView, outputView);
+        Controller.startCalculate();
     }
 }

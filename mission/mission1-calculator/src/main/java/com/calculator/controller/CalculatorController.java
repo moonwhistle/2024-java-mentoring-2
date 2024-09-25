2b4814f13a4da780434efcea5aae19815968dbc5
daeyeon <kdy4410121@naver.com>
2024-09-25 19:25:34 +0900

    feat: 계산 하는 메서드 생성

--- mission/mission1-calculator/src/main/java/com/calculator/controller/CalculatorController.java
+++ mission/mission1-calculator/src/main/java/com/calculator/controller/CalculatorController.java
@@ -1,7 +1,10 @@
 package com.calculator.controller;
 
+import com.calculator.domain.StringCalculator;
 import com.calculator.view.InputView;
 import com.calculator.view.OutputView;
+
+import java.util.List;
 
 public class CalculatorController {
     private final InputView inputView;
@@ -10,5 +13,13 @@ public class CalculatorController {
     public CalculatorController(InputView inputView, OutputView outputView){
         this.inputView = inputView;
         this.outputView = outputView;
+    }
+
+    public void startCalculate(){
+        final StringCalculator stringCalculator = new StringCalculator(inputView.input());
+        List<String> strList = stringCalculator.stringList();
+        List<Integer> integerList = stringCalculator.StringToNum(strList);
+        int result = stringCalculator.addNum(integerList);
+        outputView.printResult(result);
     }
 }

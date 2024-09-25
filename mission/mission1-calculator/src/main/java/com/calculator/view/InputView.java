0319b8a7997a86e054bb2b536622154f7aedb861
daeyeon <kdy4410121@naver.com>
2024-09-25 19:22:57 +0900

    feat: input view에 input 메서드 생성

---
+++ mission/mission1-calculator/src/main/java/com/calculator/view/InputView.java
@@ -0,0 +1,11 @@
+package com.calculator.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private final Scanner s = new Scanner(System.in);
+
+    public String input(){
+        return s.next();
+    }
+}

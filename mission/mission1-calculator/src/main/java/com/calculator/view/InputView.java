5ccd81d07f7b022f8fc0bdee4a6e46a430523f57
baekseungho <mjbsh02@mju.ac.kr>
2024-09-26 20:02:58 +0900

    feat: View 나눗셈 예외 처리 추가

--- mission/mission1-calculator/src/main/java/com/calculator/view/InputView.java
+++ mission/mission1-calculator/src/main/java/com/calculator/view/InputView.java
@@ -13,6 +13,8 @@ public class InputView {
 
     public int secondOperand() {
         System.out.println("두번째 연산자를 입력하세요.");
+        int secondOperand = sc.nextInt();
+        if (secondOperand == 0) throw new ArithmeticException("0으로 나뉘지 않습니다.");
         return sc.nextInt();
     }
 }

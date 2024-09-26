1ff0367b4e07014027486a1d728bb0762b5c203e
baekseungho <mjbsh02@mju.ac.kr>
2024-09-26 20:44:32 +0900

    feat: Controller 생성

--- mission/mission1-calculator/src/main/java/com/calculator/controller/CalculatorController.java
+++ mission/mission1-calculator/src/main/java/com/calculator/controller/CalculatorController.java
@@ -1,8 +1,30 @@
 package com.calculator.controller;
 
+import com.calculator.domain.Calculator;
+
+import com.calculator.view.InputView;
+import com.calculator.view.OutputView;
+
 public class CalculatorController {
 
-    public static void main(String[] args) {
-        System.out.println("dd");
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public CalculatorController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void controllerStart() {
+
+        Calculator calculator = new Calculator();
+
+        int firstOperand = inputView.firstOperand();
+        int secondOperand = inputView.secondOperand();
+
+        outputView.printResult(calculator.add(firstOperand, secondOperand));
+        outputView.printResult(calculator.subtract(firstOperand, secondOperand));
+        outputView.printResult(calculator.multiply(firstOperand, secondOperand));
+        outputView.printResult(calculator.divide(firstOperand, secondOperand));
     }
 }

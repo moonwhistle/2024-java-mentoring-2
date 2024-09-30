d51ae80e1d83366adb7b5aed54d4ca387f54377a
baekseungho <mjbsh02@mju.ac.kr>
2024-09-30 13:32:21 +0900

    feat: Application 동작 코드 생성

--- mission/mission1-calculator/src/main/java/com/calculator/CalculatorApplication.java
+++ mission/mission1-calculator/src/main/java/com/calculator/CalculatorApplication.java
@@ -1,7 +1,17 @@
 package com.calculator;
 
+import com.calculator.view.InputView;
+import com.calculator.view.OutputView;
+
+import com.calculator.controller.CalculatorController;
+
 public class CalculatorApplication {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        CalculatorController calculatorController = new CalculatorController(inputView, outputView);
+        calculatorController.controllerStart();
     }
 }

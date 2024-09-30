600588f2a79eab06f93a14d14fccc3c512143026
sunoo2468@gmail.com <sunoo2468@naver.com>
2024-09-30 20:34:35 +0900

    feat: 네이밍 변경, 개행 추가

--- mission/mission1-calculator/src/main/java/com/calculator/view/InputView.java
+++ mission/mission1-calculator/src/main/java/com/calculator/view/InputView.java
@@ -1,15 +1,26 @@
 package com.calculator.view;
+
 import java.util.Scanner;
 
 public class InputView {
-    Scanner sc = new Scanner(System.in);
 
-    public int inputFirst_number(){
+    Scanner scanner = new Scanner(System.in);
+
+    public int firstNumber(){
         System.out.print("Enter first number: ");
-        return sc.nextInt();
+        return scanner.nextInt();
     }
-    public int inputSecond_number(){
-        System.out.print("Enter second number: ");
-        return sc.nextInt();
+
+    public int secondNumber(){
+        while (true) {
+            System.out.print("Enter second number: ");
+            int secondNumber = scanner.nextInt();
+            if (secondNumber != 0) {
+                scanner.nextLine();
+                return secondNumber;
+            }
+        }
+
     }
+
 }

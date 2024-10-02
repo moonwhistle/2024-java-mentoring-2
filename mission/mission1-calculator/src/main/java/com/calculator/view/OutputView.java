f7bcb5a6b8174f2498f1a5dcabc6cd83f72bb1f5
sunoo2468@gmail.com <sunoo2468@naver.com>
2024-10-03 00:50:24 +0900

    feat: 개행 추가, fix: 문자열 변경

--- mission/mission1-calculator/src/main/java/com/calculator/view/OutputView.java
+++ mission/mission1-calculator/src/main/java/com/calculator/view/OutputView.java
@@ -3,7 +3,7 @@ package com.calculator.view;
 public class OutputView {
 
     public void result(int resultNumber){
-        System.out.println("Result: "+ resultNumber );
+        System.out.println("결과: "+ resultNumber );
     }
 
 }

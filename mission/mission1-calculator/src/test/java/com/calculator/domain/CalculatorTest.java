80f2ea61e247a2827df9303c319c8c45bb98215e
baekseungho <mjbsh02@mju.ac.kr>
2024-10-02 13:22:45 +0900

    refactor: 코드 정렬

--- mission/mission1-calculator/src/test/java/com/calculator/domain/CalculatorTest.java
+++ mission/mission1-calculator/src/test/java/com/calculator/domain/CalculatorTest.java
@@ -9,6 +9,7 @@ class CalculatorTest {
     @Test
     @DisplayName("덧셈 테스트")
     void addTest() {
+
         //given
         int firstOperand = 5;
         int secondOperand = 0;
@@ -25,6 +26,7 @@ class CalculatorTest {
     @Test
     @DisplayName("뺄셈 테스트")
     void subtractTest() {
+
         //given
         int firstOperand = 5;
         int secondOperand = 0;
@@ -41,6 +43,7 @@ class CalculatorTest {
     @Test
     @DisplayName("곱셈 테스트")
     void multiplyTest() {
+
         //given
         int firstOperand = 5;
         int secondOperand = 0;
@@ -57,6 +60,7 @@ class CalculatorTest {
     @Test
     @DisplayName("나눗셈 테스트")
     void divideTest() {
+
         //given
         int firstOperand = 5;
         int secondOperand = 1;
@@ -73,17 +77,16 @@ class CalculatorTest {
     @Test
     @DisplayName("나눗셈 예외 발생 테스트")
     void divideExceptionTest() {
+
         //given
         int firstOperand = 5;
         int secondOperand = 0;
         Calculator calculator = new Calculator(firstOperand, secondOperand);
         String expected = "0으로 나눌 수 없습니다.";
 
-        //when
+        //when & then
         ArithmeticException arithmeticException =
                 Assertions.assertThrows(ArithmeticException.class,() -> calculator.divide());
-
-        //then
         Assertions.assertEquals(expected, arithmeticException.getMessage());
     }
 }

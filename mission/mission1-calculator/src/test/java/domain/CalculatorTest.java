40ca4f4621121c0d09983be77241b65c63f2d9bf
hyeok232 <yshyeok30@gmail.com>
2024-10-05 20:44:16 +0900

    feat:덧셈 테스트 구현

---
+++ mission/mission1-calculator/src/test/java/domain/CalculatorTest.java
@@ -0,0 +1,26 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    @DisplayName("덧셈 테스트")
+    void 덧셈_테스트() {
+        //given
+        Calculator calculator = new Calculator();
+        int firstNumber = 1;
+        int secondNumber = 2;
+        int expected = 3;
+
+        //when
+        int result = calculator.add(firstNumber, secondNumber);
+
+        //then
+        assertEquals(expected, result);
+    }
+
+}

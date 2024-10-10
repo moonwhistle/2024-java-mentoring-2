da4cb751d894fb50dde25e85d1a793cb5de68dac
daeyeon <kdy4410121@naver.com>
2024-10-10 22:33:58 +0900

    feat: Car 객체에서 RandomNumber 인터페이스와 의존성 없앰

--- mission/mission2-racing/src/main/java/com/racing/domain/Car.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/Car.java
@@ -1,9 +1,12 @@
 package com.racing.domain;
 
 import java.util.Random;
-public class Car implements RandomNumber {
+public class Car{
 
     private final int StartPosition = 0;
+    private final int moveFront = 1;
+    private final int notMove = 0;
+    private final int bound = 4;
     private int position;
     private String carName;
 
@@ -13,15 +16,21 @@ public class Car implements RandomNumber {
         this.position = StartPosition;
     }
 
-    public int getRandomNumber(){
-        return generateRandomNumber();
-    }
-
     public void getPosition(){
         System.out.println(this.position);;
     }
 
     public void carMove(int move){
         this.position += move;
+    }
+
+    public int checkMove(int random){
+        return validateRandomNumbers(random);
+    }
+
+    public int validateRandomNumbers(final int random){
+        if(random >= bound)
+            return moveFront;
+        return notMove;
     }
 }

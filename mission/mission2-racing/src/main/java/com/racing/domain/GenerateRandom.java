75afd262933eed64d66e0c497360987659fbe9d1
daeyeon <kdy4410121@naver.com>
2024-10-10 23:09:58 +0900

    feat: GenerateRandom 인터페이스

---
+++ mission/mission2-racing/src/main/java/com/racing/domain/GenerateRandom.java
@@ -0,0 +1,12 @@
+package com.racing.domain;
+
+import java.util.Random;
+
+public interface GenerateRandom {
+
+    int limitNumber = 10;
+
+    public static Random random = new Random();
+
+    abstract int generateRandomNumber();
+}

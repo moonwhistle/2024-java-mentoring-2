4b8b99b2a551fa02125226e7111bb520c2552b9e
김동우 <t6u80o@naver.com>
2024-10-14 11:18:42 +0900

    MISSION1/STEP1 제출합니다.

---
+++ mission/mission1-calculator/src/main/java/com/calculator/controller/CalculatorController.java
@@ -0,0 +1,29 @@
+package com.calculator.controller;
+
+import com.calculator.domain.BasicCalculator;
+import com.calculator.view.InputView;
+import com.calculator.view.OutputView;
+
+public class CalculatorController {
+
+    InputView inputView;
+    OutputView outputView;
+
+    public CalculatorController(){
+
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void ProcessBasicCalculate(){
+
+        int value1 = Integer.parseInt(inputView.input());
+        int value2 = Integer.parseInt(inputView.input());
+
+        BasicCalculator basicCalculator = new BasicCalculator();
+        outputView.printValue(basicCalculator.getAdd(value1, value2));
+        outputView.printValue(basicCalculator.getMinus(value1, value2));
+        outputView.printValue(basicCalculator.getMultiply(value1, value2));
+        outputView.printValue(basicCalculator.getDivide(value1, value2));
+    }
+}
---
+++ mission/mission1-calculator/src/main/java/com/calculator/domain/BasicCalculator.java
@@ -0,0 +1,24 @@
+package com.calculator.domain;
+
+public class BasicCalculator {
+
+    public int getAdd(int a, int b){
+
+        return a + b;
+    }
+
+    public int getMinus(int a, int b){
+
+        return a - b;
+    }
+
+    public int getMultiply(int a, int b){
+
+        return a * b;
+    }
+
+    public int getDivide(int a, int b){
+
+        return a / b;
+    }
+}

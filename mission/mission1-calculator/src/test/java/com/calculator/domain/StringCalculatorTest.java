42255d17610d60b304e086785f0366578dc02079
baekseungho <mjbsh02@mju.ac.kr>
2024-10-16 20:47:28 +0900

    Refactor: 커스텀 문자열 분리 메서드 테스트 구현

--- mission/mission1-calculator/src/test/java/com/calculator/domain/StringCalculatorTest.java
+++ mission/mission1-calculator/src/test/java/com/calculator/domain/StringCalculatorTest.java
@@ -83,4 +83,19 @@ class StringCalculatorTest {
         //then
         Assertions.assertArrayEquals(expected, result);
     }
+
+    @Test
+    void 커스텀문자열분리테스트() {
+
+        //given
+        String message = "//;\n1;2;3;4";
+        String separator = ";";
+        String[] expected = {"1", "2", "3", "4"};
+
+        //when
+        String[] result = stringCalculator.separate(stringCalculator.getMessage(message), separator);
+
+        //then
+        Assertions.assertArrayEquals(expected, result);
+    }
 }

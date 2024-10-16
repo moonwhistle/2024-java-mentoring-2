95ba4990dc9df9265b7c560d304a41d3ca1112a4
parkjiwon530 <dojang120@mju.ac.kr>
2024-10-16 17:45:12 +0900

    feat: 요소 분리 메서드 feat: 구분자 추출 메서드 구현 feat: 숫자 추출 메서드 구현 feat: SeparatorCalculate 메서드 구현 Refector: 코드 전체 수정 Feat: 테스트코드 Refector: 예외처리부분 수정하였습니다 Feat: 개행 완료했습니다

--- mission/mission1-calculator/src/test/java/com/calculator/domain/SeparatorCalculatorTest.java
+++ mission/mission1-calculator/src/test/java/com/calculator/domain/SeparatorCalculatorTest.java
@@ -1,21 +1,27 @@
 package com.calculator.domain;
+
 import org.junit.jupiter.api.Assertions;
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class SeparatorCalculatorTest {
+
     @Test
     public void 커스텀_구분자_테스트() {
+
         //given
         String text = "//'\n1'2'3'4'5'6'7'8'9";
         String expectedResult = "'";
         String differentResult = ".";
+
         //when
         SeparatorCalculator separatorCalculator = new SeparatorCalculator();
         String result = separatorCalculator.selectSeparator(text);
+
         //then
         Assertions.assertAll(
                 () -> assertEquals(expectedResult, result),
@@ -25,13 +31,16 @@ class SeparatorCalculatorTest {
 
     @Test
     public void 숫자부분_문자열_테스트() {
+
         //given
         String text = "//:\n1:2:3:4:5:6:7:8:9";
         String expectedResult = "1:2:3:4:5:6:7:8:9";
         String differentResult = "1,2,3,4,5,6,7,8,9";
+
         //when
         SeparatorCalculator separatorCalculator = new SeparatorCalculator();
         String result = separatorCalculator.numberText(text);
+
         //then
         Assertions.assertAll(
                 () -> assertEquals(expectedResult, result),
@@ -40,14 +49,17 @@ class SeparatorCalculatorTest {
     }
     @Test
     public void 음수_예외처리_테스트() {
+
         //given
         String text = "1!2!3!-4!5!6!7!8!9";
         String separator = "!";
         String expectedResult = "숫자가 음수입니다.";
         int differentResult = 41;
+
         //when
         SeparatorCalculator separatorCalculator = new SeparatorCalculator();
         int result = separatorCalculator.calculatorSum(text,separator);
+
         //then
         Assertions.assertAll(
                 () -> assertEquals(expectedResult, result),
@@ -57,20 +69,22 @@ class SeparatorCalculatorTest {
 
     @Test
     public void calculateTest() {
+
         //given
         String text = "1,2,3,4,5,6,7,8,9";
         String separator = ",";
         int expectedResult = 45;
         int differentResult = 5;
+
         //when
         SeparatorCalculator separatorCalculator = new SeparatorCalculator();
         int result = separatorCalculator.calculatorSum(text,separator);
+
         //then
         System.out.println(result);
         Assertions.assertAll(
                 () -> assertEquals(expectedResult, result),
                 () -> assertNotEquals(differentResult, result)
-
         );
     }
 

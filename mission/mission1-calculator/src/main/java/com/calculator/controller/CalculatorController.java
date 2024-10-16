1907abf47f2014113ac8969ccfd656ade6aa3b1b
baekseungho <mjbsh02@mju.ac.kr>
2024-10-16 16:29:24 +0900

    Refactor: controller 생성자 수정

--- mission/mission1-calculator/src/main/java/com/calculator/controller/CalculatorController.java
+++ mission/mission1-calculator/src/main/java/com/calculator/controller/CalculatorController.java
@@ -10,18 +10,20 @@ public class CalculatorController {
     private final InputView inputView;
     private final OutputView outputView;
 
-    public CalculatorController(InputView inputView, OutputView outputView) {
+    private final Calculator calculator;
+
+    public CalculatorController(InputView inputView, OutputView outputView, Calculator calculator) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.calculator = calculator;
     }
 
     public void controllerStart() {
         int firstOperand = inputView.firstOperand();
         int secondOperand = inputView.secondOperand();
-        Calculator calculator = new Calculator(firstOperand, secondOperand);
-        outputView.printResult(calculator.add());
-        outputView.printResult(calculator.subtract());
-        outputView.printResult(calculator.multiply());
-        outputView.printResult(calculator.divide());
+        outputView.printResult(calculator.add(firstOperand, secondOperand));
+        outputView.printResult(calculator.subtract(firstOperand, secondOperand));
+        outputView.printResult(calculator.multiply(firstOperand, secondOperand));
+        outputView.printResult(calculator.divide(firstOperand, secondOperand));
     }
 }

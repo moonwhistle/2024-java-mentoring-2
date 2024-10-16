5dacfd7d647804520b774c5b48b1e9faf12f04fc
baekseungho <mjbsh02@mju.ac.kr>
2024-10-16 16:30:47 +0900

    Refactor: CalculatorApplication 수정

--- mission/mission1-calculator/src/main/java/com/calculator/CalculatorApplication.java
+++ mission/mission1-calculator/src/main/java/com/calculator/CalculatorApplication.java
@@ -3,13 +3,16 @@ package com.calculator;
 import com.calculator.view.InputView;
 import com.calculator.view.OutputView;
 
+import com.calculator.domain.Calculator;
+
 import com.calculator.controller.CalculatorController;
 
 public class CalculatorApplication {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
-        CalculatorController calculatorController = new CalculatorController(inputView, outputView);
+        Calculator calculator = new Calculator();
+        CalculatorController calculatorController = new CalculatorController(inputView, outputView, calculator);
         calculatorController.controllerStart();
     }
 }

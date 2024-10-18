5e1d2d1e9f6b031381fa4e4162ed26f836f391a3
김동우 <t6u80o@naver.com>
2024-10-18 11:18:22 +0900

    MISSION1/STEP3 피드백 반영한 수정사항입니다.

--- mission/mission1-calculator/src/main/java/com/calculator/domain/StringCalculator.java
+++ mission/mission1-calculator/src/main/java/com/calculator/domain/StringCalculator.java
@@ -1,12 +1,15 @@
 package com.calculator.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class StringCalculator {
 
     private final String START_STRING = "//";
     private final String REPLACE_STRING = ",";
+    private final String SLICE_STRING = "n";
+    private final String[] NORMAL_SEPERATOR_ARRAY = {",", ":"};
 
     private final String SEPARATOR_ERROR_MESSAGE = "확인되지 않은 구분자입니다.";
     private final String MINUS_ERROR_MESSAGE = "음수입니다.";
@@ -15,19 +18,15 @@ public class StringCalculator {
         List<String> seperatorList = getSeperatorList(stringValue);
 
         stringValue = tuneStringValue(stringValue);
-
-        for (String seperator : seperatorList) {
-            stringValue = stringValue.replaceAll(seperator, REPLACE_STRING);
-        }
-
+        stringValue = generalizeSeperator(seperatorList, stringValue);
+        
         return getSum(stringValue);
     }
 
     public List<String> getSeperatorList(String stringValue){
         List<String> seperatorList = new ArrayList<>();
 
-        seperatorList.add(",");
-        seperatorList.add(":");
+        Collections.addAll(seperatorList, NORMAL_SEPERATOR_ARRAY);
 
         if (stringValue.startsWith(START_STRING)){
             seperatorList.add(stringValue.substring(2, 3));
@@ -38,9 +37,16 @@ public class StringCalculator {
 
     public String tuneStringValue(String stringValue){
         if (stringValue.startsWith(START_STRING)) {
-            String SLICE_STRING = "n";
             int sliceIndex = stringValue.indexOf(SLICE_STRING) + 1;
             stringValue = stringValue.substring(sliceIndex);
+        }
+
+        return stringValue;
+    }
+
+    public String generalizeSeperator(List<String> seperatorList, String stringValue){
+        for (String seperator : seperatorList) {
+            stringValue = stringValue.replaceAll(seperator, REPLACE_STRING);
         }
 
         return stringValue;
@@ -58,18 +64,19 @@ public class StringCalculator {
         return sum;
     }
 
-    public int getParsedInteger(String nums) {
+    public int getParsedInteger(String str) {
         try {
-            return isMinus(Integer.parseInt(nums));
+            return isMinus(Integer.parseInt(str));
         } catch (NumberFormatException e) {
             throw new RuntimeException(SEPARATOR_ERROR_MESSAGE);
         }
     }
 
-    private int isMinus(int testNum) {
-        if (testNum < 0) {
+    private int isMinus(int num) {
+        if (num < 0) {
             throw new RuntimeException(MINUS_ERROR_MESSAGE);
         }
-        return testNum;
+
+        return num;
     }
 }

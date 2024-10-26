213f8142e5b066a9802e6c99028c41230479068f
baekseungho <mjbsh02@mju.ac.kr>
2024-10-26 18:02:59 +0900

    Feat: 랜덤 값 구하는 메서드 구현

--- mission/mission2-racing/src/main/java/com/racing/domain/Car.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/Car.java
@@ -1,4 +1,15 @@
 package com.racing.domain;
 
+import java.util.Random;
+
 public class Car {
+
+    public String showCarName(String carName) {
+        return carName;
+    }
+
+    private int getRandomNumber() {
+        Random randomNumber = new Random();
+        return randomNumber.nextInt(9);
+    }
 }

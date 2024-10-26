093fed64bcb68491ff0fbc31bc7a9d0a0a277304
baekseungho <mjbsh02@mju.ac.kr>
2024-10-26 17:33:56 +0900

    Feat: 차 이름 입력 메서드 생성

--- mission/mission2-racing/src/main/java/com/racing/view/InputView.java
+++ mission/mission2-racing/src/main/java/com/racing/view/InputView.java
@@ -6,4 +6,8 @@ public class InputView {
 
     private final Scanner scanner = new Scanner(System.in);
 
+    public String carName() {
+        System.out.print("차의 이름을 입력하세요");
+        return scanner.next();
+    }
 }

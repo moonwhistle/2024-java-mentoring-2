e8f7966b22acf91487ebff6d94fd3d0da2af549b
baekseungho <mjbsh02@mju.ac.kr>
2024-10-26 19:27:11 +0900

    Feat: Car 이름과 상태 String[] 저장 메서드 생성

--- mission/mission2-racing/src/main/java/com/racing/domain/Car.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/Car.java
@@ -14,6 +14,17 @@ public class Car {
         return INITIAL_STATE;
     }
 
+    public String[] showCarState(String carName, String state) {
+        return carState(carName, state);
+    }
+
+    private static String[] carState(String carName, String state) {
+        String[] states = new String[2];
+        states[0] = carName;
+        states[1] = INITIAL_STATE;
+        return states;
+    }
+
     private int getRandomNumber() {
         Random randomNumber = new Random();
         return randomNumber.nextInt(9);

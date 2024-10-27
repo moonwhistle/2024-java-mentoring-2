e2a16fd534e31a1fff93dc3e5679bec043eb66fa
baekseungho <mjbsh02@mju.ac.kr>
2024-10-27 16:01:33 +0900

    Feat: 차 이 배열 저장 테스트

--- mission/mission2-racing/src/test/java/com/racing/domain/CarTest.java
+++ mission/mission2-racing/src/test/java/com/racing/domain/CarTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class CarTest {
 
     private Car car;
@@ -26,4 +29,23 @@ class CarTest {
         //then
         Assertions.assertEquals(expected, result);
     }
+
+    @Test
+    void 차이름배열저장테스트() {
+
+        //given
+        String carName = "Daytona";
+        List<String> carInformation = new ArrayList<>();
+        carInformation.add(carName);
+        String[] expected = carInformation.toArray(new String[carInformation.size()]);
+
+        //when
+        List<String> saving = car.saveCar(carName);
+        String[] result = saving.toArray(new String[saving.size()]);
+
+        //then
+        Assertions.assertArrayEquals(expected, result);
+    }
 }
+
+

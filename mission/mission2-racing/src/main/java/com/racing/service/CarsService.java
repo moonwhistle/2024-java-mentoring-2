a0b53888a29a932852bff772304919fcde5737a0
daeyeon <kdy4410121@naver.com>
2024-10-29 15:22:01 +0900

    feat: carsService 생성, 생성자 구현

---
+++ mission/mission2-racing/src/main/java/com/racing/service/CarsService.java
@@ -0,0 +1,16 @@
+package com.racing.service;
+
+import com.racing.domain.Cars;
+
+public class CarsService {
+    private final int startIndex = 0;
+
+    private Cars cars;
+
+    public CarsService(Cars cars) {
+        this.cars = cars;
+    }
+
+
+
+}

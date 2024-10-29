48f4a92458907e54823342233542c24b0ae7aff7
daeyeon <kdy4410121@naver.com>
2024-10-29 15:24:21 +0900

    feat: 우승자 정해주는 winnerList, getMaxPosition 함수 구현

--- mission/mission2-racing/src/main/java/com/racing/service/CarsService.java
+++ mission/mission2-racing/src/main/java/com/racing/service/CarsService.java
@@ -2,6 +2,9 @@ package com.racing.service;
 
 import com.racing.domain.Cars;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarsService {
     private final int startIndex = 0;
 
@@ -26,6 +29,26 @@ public class CarsService {
         }
 
         return carDetail.toString();
+    }
+
+    public List<String> winnerList(){
+        List<String> winner = new ArrayList<>();
+        for(int i = startIndex; i < cars.getCarsSize(); i++) {
+            if(cars.getCarsPosition().get(i) == getMaxPosition())
+                winner.add(cars.getCarsName().get(i));
+        }
+
+        return winner;
+    }
+
+    private int getMaxPosition(){
+        int max = 0;
+        for(int i = startIndex; i < cars.getCarsSize(); i++) {
+            if(max < cars.getCarsPosition().get(i))
+                max = cars.getCarsPosition().get(i);
+        }
+
+        return max;
     }
 
 }

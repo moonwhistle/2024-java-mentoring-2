03d79c9d897c535bf08589b952e75c833582f79f
daeyeon <kdy4410121@naver.com>
2024-10-29 15:35:18 +0900

    feat: Randomnumber를 여러개 발생시키고 list에 저장하는 함수 생성

--- mission/mission2-racing/src/main/java/com/racing/domain/RandomNumbers.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/RandomNumbers.java
@@ -1,10 +1,22 @@
 package com.racing.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RandomNumbers implements GenerateRandom {
+    private final int zeroIndex = 0;
 
     @Override
     public int generateRandomNumber(){
         return random.nextInt(limitNumber);
     }
 
+    @Override
+    public List<Integer> randomNumberList(int number){
+        List<Integer> randomList = new ArrayList<>();
+        for(int i = zeroIndex; i < number; i++){
+            randomList.add(random.nextInt());
+        }
+        return randomList;
+    }
 }

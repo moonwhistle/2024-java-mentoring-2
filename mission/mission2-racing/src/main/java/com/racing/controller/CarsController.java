abf3842191757d48e27f138b1c6e1a07a8621c02
daeyeon <kdy4410121@naver.com>
2024-10-29 15:33:08 +0900

    feat: 레이싱 시작하는 startCarsMove함수 생성

--- mission/mission2-racing/src/main/java/com/racing/controller/CarsController.java
+++ mission/mission2-racing/src/main/java/com/racing/controller/CarsController.java
@@ -1,10 +1,13 @@
 package com.racing.controller;
 
 import com.racing.domain.Car;
+import com.racing.domain.Cars;
 import com.racing.domain.GenerateRandom;
 import com.racing.service.CarsService;
 import com.racing.view.InputView;
 import com.racing.view.OutputView;
+
+import java.util.List;
 
 public class CarsController {
 
@@ -19,6 +22,22 @@ public class CarsController {
         this.outputView = outputView;
         this.generateRandom = generateRandom;
         this.carsService = carsService;
+    }
+
+    public void startCarsMove(){
+        List<String> carNames = inputView.inputName();
+        Cars cars = new Cars(carNames, generateRandom);
+        this.carsService = new CarsService(cars);
+        int numberOfMove = inputView.integerInput();
+        outputView.printDefault();
+
+        for(int i = startIndex; i < numberOfMove; i++){
+            cars.carsMove();
+            outputView.printCarsMove(carsService.carsDetail());
+        }
+
+        List<String> winner = carsService.winnerList();
+        outputView.printWinner(winner);
     }
 
 

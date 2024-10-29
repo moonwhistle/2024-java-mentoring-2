50fc94309dc7e28cb7848cafb90e4e5297c473ed
baekseungho <mjbsh02@mju.ac.kr>
2024-10-29 17:04:49 +0900

    Refactor: 함수명 수정, 움직이는 함수 간소화

--- mission/mission2-racing/src/main/java/com/racing/domain/Car.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/Car.java
@@ -8,6 +8,7 @@ import java.util.Random;
 public class Car {
 
     private static final String MOVING_POINT = "-";
+    private static final int DECISION_POINT = 4;
 
     public List<String> saveCar(String carName) {
         List<String> car = new ArrayList<>();
@@ -16,18 +17,17 @@ public class Car {
     }
 
     public int receiveNumber() {
-        return getRandomNumber();
+        return generateRandomNumber();
     }
 
     public List<String> forwardOrStop(List<String> carInformation, int randomNumber) {
-        if (randomNumber >= 4) {
+        if (randomNumber >= DECISION_POINT) {
             carInformation.add(MOVING_POINT);
-            return carInformation;
         }
         return carInformation;
     }
 
-    private int getRandomNumber() {
+    private int generateRandomNumber() {
         Random randomNumber = new Random();
         return randomNumber.nextInt(9);
     }

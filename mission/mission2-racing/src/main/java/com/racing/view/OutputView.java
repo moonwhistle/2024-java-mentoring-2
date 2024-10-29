f1496bc8ebd5e133a8330a2b21f4acf7a7b236c9
daeyeon <kdy4410121@naver.com>
2024-10-29 15:29:49 +0900

    feat: 형식에 맞는 출력 함수 생성

--- mission/mission2-racing/src/main/java/com/racing/view/OutputView.java
+++ mission/mission2-racing/src/main/java/com/racing/view/OutputView.java
@@ -1,9 +1,21 @@
 package com.racing.view;
 
-public class OutputView {
+import java.util.List;
 
-    public void printResult(int number){
-        System.out.println(number);
+public class OutputView {
+    private final int startIndex = 0;
+
+    public void printDefault(){
+        System.out.println();
+        System.out.println("실행 결과");
+    }
+
+    public void printCarsMove(String carDetailList){
+        System.out.println(carDetailList);
+    }
+
+    public void printWinner(List<String> winner){
+        System.out.println("최종 우승자 : " + String.join(",", winner) + "가 최종 우승했습니다.");
     }
 
 }

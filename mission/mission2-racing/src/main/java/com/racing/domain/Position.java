e801bda950e71bb2fcf89b1db24cf64275ee1884
daeyeon <kdy4410121@naver.com>
2024-10-31 11:24:30 +0900

    feat: 원시값 포장을 위한 Position 도메인 생성

---
+++ mission/mission2-racing/src/main/java/com/racing/domain/Position.java
@@ -0,0 +1,15 @@
+package com.racing.domain;
+
+public class Position {
+
+    private int position;
+
+    public Position(int position){
+        this.position = position;
+    }
+
+    public void movePosition(){
+        this.position++;
+    }
+
+}

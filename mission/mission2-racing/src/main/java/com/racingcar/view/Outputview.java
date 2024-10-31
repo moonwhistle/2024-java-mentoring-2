1ceb87f82b0d51b662fe062a05e3ef5a520d9e64
김동우 <t6u80o@naver.com>
2024-10-31 16:23:35 +0900

    view 경우에 맞는 print 메서드 추가

--- mission/mission2-racing/src/main/java/com/racingcar/view/Outputview.java
+++ mission/mission2-racing/src/main/java/com/racingcar/view/Outputview.java
@@ -2,7 +2,11 @@ package com.racingcar.view;
 
 public class Outputview {
 
-    public void printValue(String value){
-        System.out.println(value);
+    public void showCarNameInputPrompt(){
+        System.out.println("경주할 자동차 이름을 입력하세요.");
+    }
+
+    public void showCarMove(String carName, int moveState){
+        System.out.println(carName + " : " + moveState);
     }
 }

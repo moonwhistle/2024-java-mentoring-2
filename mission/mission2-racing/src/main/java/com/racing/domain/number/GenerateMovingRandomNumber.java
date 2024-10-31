9e4849ea10093e65884e1d53e53f5e4e30d3a637
baekseungho <mjbsh02@mju.ac.kr>
2024-10-31 16:32:39 +0900

    Refactor: 랜덤 생성 클래스 네이밍 수정

--- mission/mission2-racing/src/main/java/com/racing/domain/number/GenerateMovingRandomNumber.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/number/GenerateMovingRandomNumber.java
@@ -1,8 +1,5 @@
 package com.racing.domain.number;
 
-import com.racing.domain.exception.NegativeRandomNumberException;
-import com.racing.domain.exception.OutOfRangeException;
-
 import java.util.Random;
 
 public class GenerateMovingRandomNumber implements RandomNumber {
@@ -14,26 +11,8 @@ public class GenerateMovingRandomNumber implements RandomNumber {
     }
 
     @Override
-    public int generateRandomNumber() {
-        return random.nextInt(9);
+    public int generateRandomNumber(int bound) {
+        return random.nextInt(bound+1);
     }
 
-    public int decideMove(int number, int state) {
-        if (number >= 4) {
-            return state++;
-        }
-        return state;
-    }
-
-    public void validateNegative(int number) {
-        if (number < 0) {
-            throw new NegativeRandomNumberException();
-        }
-    }
-
-    public void validateOverBound(int number) {
-        if (number > 9) {
-            throw new OutOfRangeException();
-        }
-    }
 }

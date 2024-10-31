bc60f50b990b90d741d9b3e7e2811812fca40b1a
김동우 <t6u80o@naver.com>
2024-10-31 15:14:30 +0900

    step1 문제 조건에 맞는 부분만 코드를 구성할 수 있도록 코드 초기화

--- mission/mission2-racing/src/main/java/com/racingcar/controller/RacingcarContoller.java
+++ mission/mission2-racing/src/main/java/com/racingcar/controller/RacingcarContoller.java
@@ -16,19 +16,11 @@ public class RacingcarContoller {
 
     public void runCar(){
 
-        outputview.printValue("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String carName = inputView.getInputValue();
-
-        outputview.printValue("시도할 회수는 몇회인가요?");
-        String tryCount = inputView.getInputValue();
+        outputview.printValue("경주할 자동차 이름을 입력하세요.");
+        String carName = inputView.getInput();
 
         RacingCarLogic racingCarLogic = new RacingCarLogic();
 
-        int integerTrycount = racingCarLogic.parseStringTryCount(tryCount);
 
-        String carMoveResult = racingCarLogic.processMoving(carName, integerTrycount);
-
-        outputview.printValue("실행 결과");
-        outputview.printValue(carMoveResult);
     }
 }

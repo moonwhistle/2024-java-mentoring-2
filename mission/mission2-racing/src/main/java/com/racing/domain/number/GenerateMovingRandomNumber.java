2a155c9e626fe107b1431a0dcdbb463a253f81ab
baekseungho <mjbsh02@mju.ac.kr>
2024-10-31 16:11:25 +0900

    Feat: 랜덤 변수 생성클래스 구현

---
+++ mission/mission2-racing/src/main/java/com/racing/domain/number/GenerateMovingRandomNumber.java
@@ -0,0 +1,39 @@
+package com.racing.domain.number;
+
+import com.racing.domain.exception.NegativeRandomNumberException;
+import com.racing.domain.exception.OutOfRangeException;
+
+import java.util.Random;
+
+public class GenerateMovingRandomNumber implements RandomNumber {
+
+    private final Random random;
+
+    public GenerateMovingRandomNumber() {
+        this.random = new Random();
+    }
+
+    @Override
+    public int generateRandomNumber() {
+        return random.nextInt(9);
+    }
+
+    public int decideMove(int number, int state) {
+        if (number >= 4) {
+            return state++;
+        }
+        return state;
+    }
+
+    public void validateNegative(int number) {
+        if (number < 0) {
+            throw new NegativeRandomNumberException();
+        }
+    }
+
+    public void validateOverBound(int number) {
+        if (number > 9) {
+            throw new OutOfRangeException();
+        }
+    }
+}

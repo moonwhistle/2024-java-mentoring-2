3acedcdecd379b8afdc209c168ec62321c697a78
김동우 <t6u80o@naver.com>
2024-10-31 15:12:45 +0900

    view input 메서드 이름 변경

--- mission/mission2-racing/src/main/java/com/racingcar/view/InputView.java
+++ mission/mission2-racing/src/main/java/com/racingcar/view/InputView.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class InputView {
     private final Scanner scanner = new Scanner(System.in);
 
-    public String getInputValue(){
+    public String getInput(){
         return scanner.next();
     }
 }

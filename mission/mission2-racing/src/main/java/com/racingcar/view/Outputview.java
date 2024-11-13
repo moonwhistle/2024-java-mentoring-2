e88c2b72c939074aa6aab486129d660fe2b075a9
김동우 <t6u80o@naver.com>
2024-11-13 18:54:32 +0900

    우승자 문자열로 출력하는 메서드 추가

--- mission/mission2-racing/src/main/java/com/racingcar/view/Outputview.java
+++ mission/mission2-racing/src/main/java/com/racingcar/view/Outputview.java
@@ -10,6 +10,10 @@ public class Outputview {
         System.out.println("시도할 횟수는 몇회인가요?");
     }
 
+    public void showWinner(String winner){
+        System.out.println(winner + "가 최종 우승했습니다.");
+    }
+
     public void showCarMove(String carName, int carMove){
         System.out.println(carName + " : " + carMove);
     }

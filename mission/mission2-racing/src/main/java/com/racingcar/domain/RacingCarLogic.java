b584b4bbbf4542b7a3144d129294c5cd4d4867e1
김동우 <t6u80o@naver.com>
2024-11-13 12:59:27 +0900

    try count parse 메소드 추가 및 test

--- mission/mission2-racing/src/main/java/com/racingcar/domain/RacingCarLogic.java
+++ mission/mission2-racing/src/main/java/com/racingcar/domain/RacingCarLogic.java
@@ -2,6 +2,38 @@ package com.racingcar.domain;
 
 public class RacingCarLogic {
 
+    private final String NAN_ERROR_MESSAGE = "입력된 값이 숫자가 아닙니다.";
+    private final String NOT_INTEGER_ERROR_MESSAGE = "입력된 값이 정수가 아닙니다.";
+    private final String MINUS_ERROR_MESSAGE = "입력된 값이 음수입니다.";
+
+    public int parseTryCount(String str) {
+        try {
+            int integerStr = isInteger(str);
+            return isMinus(integerStr);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException(NAN_ERROR_MESSAGE);
+        }
+    }
+
+    private int isMinus(int num) {
+        if (num < 0) {
+            throw new RuntimeException(MINUS_ERROR_MESSAGE);
+        }
+
+        return num;
+    }
+
+    private int isInteger(String str) {
+
+        double doubleStr = Double.parseDouble(str);
+
+        if(doubleStr != (int) doubleStr){
+            throw new RuntimeException(NOT_INTEGER_ERROR_MESSAGE);
+        }
+
+        return (int) doubleStr;
+    }
+
     public int getCarMoveState(int randomNumber){
         if(randomNumber >= 4){
             return 1;
--- mission/mission2-racing/src/test/java/TestRacingCar.java
+++ mission/mission2-racing/src/test/java/TestRacingCar.java
@@ -2,6 +2,8 @@ import com.racingcar.domain.RacingCarLogic;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestRacingCar {
     @Test
     public void testMoveState() {
@@ -37,5 +39,60 @@ public class TestRacingCar {
         //then
         Assertions.assertEquals(result, expected);
         Assertions.assertNotEquals(result, notExpected);
+    }
+
+    @Test
+    public void notIntegerErrorTest() {
+
+        //given
+        String strNumber = "1.5";
+
+        String expectedErrorMessage = "입력된 값이 정수가 아닙니다.";
+
+        //when
+        RacingCarLogic racingCarLogic = new RacingCarLogic();
+
+        RuntimeException thrown = Assertions.assertThrows(RuntimeException.class,
+                () -> racingCarLogic.parseTryCount(strNumber)
+        );
+
+        //then
+        assertEquals(expectedErrorMessage, thrown.getMessage());
+    }
+
+    @Test
+    public void minusErrorTest() {
+
+        //given
+        String strNumber = "-1";
+
+        String expectedErrorMessage = "입력된 값이 음수입니다.";
+
+        //when
+        RacingCarLogic racingCarLogic = new RacingCarLogic();
+
+        RuntimeException thrown = Assertions.assertThrows(RuntimeException.class,
+                () -> racingCarLogic.parseTryCount(strNumber)
+        );
+
+        //then
+        assertEquals(expectedErrorMessage, thrown.getMessage());
+    }
+
+    @Test
+    public void parseIntTest() {
+
+        //given
+        String strNumber = "1";
+
+        int expected = 1;
+
+        //when
+        RacingCarLogic racingCarLogic = new RacingCarLogic();
+
+        int result = racingCarLogic.parseTryCount(strNumber);
+
+        //then
+        assertEquals(expected,result);
     }
 }

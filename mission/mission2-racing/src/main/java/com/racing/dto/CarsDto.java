37035b7f329bde207eafa3030c50bfd54e932ff8
daeyeon <kdy4410121@naver.com>
2024-11-16 15:08:43 +0900

    feat: dto 도메인 record로 생성

--- mission/mission2-racing/src/main/java/com/racing/dto/CarsDto.java
+++ mission/mission2-racing/src/main/java/com/racing/dto/CarsDto.java
@@ -1,27 +1,7 @@
 package com.racing.dto;
 
-import com.racing.domain.Car;
 import com.racing.domain.GenerateRandom;
 import com.racing.domain.Name;
 
-import java.util.List;
+public record CarsDto(Name carName, GenerateRandom generateRandom) {}
 
-public class CarsDto {
-
-    private final Name carName;
-    private final GenerateRandom generateRandom;
-
-    public CarsDto(Name carName, GenerateRandom generateRandom){
-        this.carName = carName;
-        this.generateRandom = generateRandom;
-    }
-
-    public Name getCarName(){
-        return carName;
-    }
-
-    public GenerateRandom getGenerateRandom(){
-        return generateRandom;
-    }
-
-}

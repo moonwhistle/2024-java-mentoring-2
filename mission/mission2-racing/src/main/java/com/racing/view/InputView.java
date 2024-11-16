0faec2398045ea6ce42b651f641254f05b96d13d
daeyeon <kdy4410121@naver.com>
2024-11-16 15:09:51 +0900

    fix: 예외처리 추가 및 split기능 도메인으로 이동

--- mission/mission2-racing/src/main/java/com/racing/view/InputView.java
+++ mission/mission2-racing/src/main/java/com/racing/view/InputView.java
@@ -8,30 +8,28 @@ public class InputView {
 
     private final Scanner scanner = new Scanner(System.in);
     private final int limitNumber = 6;
-    private final String splitString = ",";
     private final int startNum = 0;
 
     public String input(){
         return scanner.next();
     }
 
-    public int IntegerInput(){
-        return scanner.nextInt();
-    }
-
-    public List<String> inputName(){
+    public String inputName(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = scanner.next();
-        String[] inputs = input.split(splitString);
 
-        return List.of(inputs);
+        return input;
     }
 
     public int integerInput(){
         System.out.println("시도할 횟수는 몇 회인가요?");
-        int input = scanner.nextInt();
-
-        return input;
+        try{
+            int input = scanner.nextInt();
+            return input;
+        }
+        catch(IllegalArgumentException e){
+            throw new IllegalArgumentException("정수 형태를 입력해야합니다.");
+        }
     }
 
 }

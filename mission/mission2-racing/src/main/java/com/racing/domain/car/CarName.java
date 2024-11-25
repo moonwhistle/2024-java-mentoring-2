dfe61773e2f36df2022800a494a8609bf53e1902
baekseungho <mjbsh02@mju.ac.kr>
2024-11-25 22:53:17 +0900

    Feat: 이름에 대한 빈 값 예외 처리 추가

--- mission/mission2-racing/src/main/java/com/racing/domain/car/CarName.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/car/CarName.java
@@ -4,6 +4,13 @@ public class CarName {
     private final String carName;
 
     public CarName(String carName) {
+        validateNameValue();
         this.carName = carName;
+    }
+
+    private void validateNameValue() {
+        if (carName == null || carName.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름을 입력하세요.");
+        }
     }
 }

0ff155205b0a5af3305c99eefcf845be942b81ca
baekseungho <mjbsh02@mju.ac.kr>
2024-11-26 21:34:31 +0900

    Refactor: Car 생성자에 state를 빼고 state는 초기 선언에서 0이 되도록 수정하여 Car 생성자에는 차 이름만 담도록 수정

--- mission/mission2-racing/src/main/java/com/racing/domain/car/Car.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/car/Car.java
@@ -2,11 +2,10 @@ package com.racing.domain.car;
 
 public class Car {
 
-    private int state;
+    private int state = 0;
     private final CarName carName;
 
     public Car(String carName) {
-        state = 0;
         this.carName = new CarName(carName);
     }
 

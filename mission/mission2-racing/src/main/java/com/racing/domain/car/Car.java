6f457cbca07d2ad2305836522e100cbd42f75726
baekseungho <mjbsh02@mju.ac.kr>
2024-12-20 04:07:26 +0900

    Feat: Car의 이름과 움직임 상태 변수 생성
    이름에 대한 책임은 CarName에게 있으므로 원시값인 String으로 선언하지 않고 자동차 이름을 관리하는 carName으로 맴버변수 설정하고 생성자에서 필드의 carName에 전달받은 차의 이름을 관리하는 CarName객체를 생성하여 저장한다.

--- mission/mission2-racing/src/main/java/com/racing/domain/car/Car.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/car/Car.java
@@ -1,3 +1,13 @@
 package com.racing.domain.car;
 
-public class Car {}
+public class Car {
+
+    private int state;
+    private final CarName carName;
+
+    public Car(final String carName) {
+        state = 0;
+        this.carName = new CarName(carName);
+    }
+
+}

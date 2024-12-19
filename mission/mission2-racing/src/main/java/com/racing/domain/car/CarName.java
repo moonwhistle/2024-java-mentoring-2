91ee4264b473f87ce95c6820e7cbc9d84c0bebcb
baekseungho <mjbsh02@mju.ac.kr>
2024-12-20 03:33:15 +0900

    Feat: 차 이름 검증 메서드 추가

--- mission/mission2-racing/src/main/java/com/racing/domain/car/CarName.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/car/CarName.java
@@ -5,6 +5,13 @@ public class CarName {
     private final String carName;
 
     public CarName(final String carName) {
+        validateCarName(carName);
         this.carName = carName;
+    }
+
+    private void validateCarName(String carName) {
+        if (carName == null) {
+            throw new IllegalArgumentException("차 이름을 다시 입력하세요.");
+        }
     }
 }

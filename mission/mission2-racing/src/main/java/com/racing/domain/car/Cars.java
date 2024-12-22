14092eae5fadc97dfd955cf1cbe1c0557ff5dbf4
baekseungho <mjbsh02@mju.ac.kr>
2024-12-23 02:38:23 +0900

    Feat: n대의 Car를 관리하는 Cars 클래스 구현

--- mission/mission2-racing/src/main/java/com/racing/domain/car/Cars.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/car/Cars.java
@@ -1,5 +1,13 @@
 package com.racing.domain.car;
 
+import java.util.List;
+
 public class Cars {
+
+    private final List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
 
 }

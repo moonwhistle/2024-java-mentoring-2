fd3234469e58cac7f5a261243b1fa9476187f3b9
baekseungho <mjbsh02@mju.ac.kr>
2024-12-23 16:45:17 +0900

    Feat: 여러 대의 차를 움직이는 메서드와 정해진 랩수만큼 차들을 움직이는 메서드 구현

--- mission/mission2-racing/src/main/java/com/racing/domain/car/Cars.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/car/Cars.java
@@ -1,5 +1,7 @@
 package com.racing.domain.car;
 
+import com.racing.domain.number.RandomNumberGenerator;
+
 import java.util.List;
 
 public class Cars {
@@ -10,4 +12,16 @@ public class Cars {
         this.cars = cars;
     }
 
+    public void racing(RandomNumberGenerator randomNumberGenerator) {
+        int numberOfLaps = 4;
+        for (int i = 0; i < numberOfLaps; i++) {
+            driveCars(randomNumberGenerator);
+        }
+    }
+
+    private void driveCars(RandomNumberGenerator randomNumberGenerator) {
+        for (Car car : cars) {
+            car.driveCar(randomNumberGenerator.generateRandomNumber());
+        }
+    }
 }

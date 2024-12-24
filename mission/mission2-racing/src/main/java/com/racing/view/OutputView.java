a1245e59d505c4aa8f8c8d58eea702af1ec26898
baekseungho <mjbsh02@mju.ac.kr>
2024-12-24 12:59:15 +0900

    Feat: 레이싱 참가 차량들의 이름과 최종 위치, 우승 자동차 출력 메서드 구현

--- mission/mission2-racing/src/main/java/com/racing/view/OutputView.java
+++ mission/mission2-racing/src/main/java/com/racing/view/OutputView.java
@@ -1,11 +1,25 @@
 package com.racing.view;
 
+import com.racing.domain.car.Car;
+
 import java.util.List;
 
 public class OutputView {
 
-    public void racingResult(List<String> winner) {
-        System.out.println(winner + "우승");
+    public void showRacingCarsName(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getCarName());
+        }
+    }
+
+    public void showCarsPosition(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getPosition());
+        }
+    }
+
+    public void showWinner(List<String> winner) {
+        System.out.println(winner);
     }
 
 }

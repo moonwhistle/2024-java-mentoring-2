ff8f1b89b452811094bdc192b28ba2e39be5c241
baekseungho <mjbsh02@mju.ac.kr>
2024-12-24 13:01:30 +0900

    Feat: 레이싱게임 구현

--- mission/mission2-racing/src/main/java/com/racing/CarApplication.java
+++ mission/mission2-racing/src/main/java/com/racing/CarApplication.java
@@ -1,15 +1,16 @@
 package com.racing;
 
 import com.racing.controller.CarController;
-import com.racing.domain.car.MovingCar;
+
 import com.racing.domain.number.RandomNumberGenerator;
+
 import com.racing.view.InputView;
 import com.racing.view.OutputView;
 
 public class CarApplication {
 
     public static void main(String[] args) {
-        CarController carController = new CarController(new RandomNumberGenerator(), new OutputView(), new InputView(), new MovingCar());
-        carController.moveCar();
+        CarController carController = new CarController(new RandomNumberGenerator(), new OutputView(), new InputView());
+        carController.run();
     }
 }

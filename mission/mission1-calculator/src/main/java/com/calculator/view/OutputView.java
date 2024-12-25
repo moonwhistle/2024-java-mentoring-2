66d40da07c84be40166e31a1b673d645966394a0
ukheeDokey <0514uh@gmail.com>
2024-12-25 19:30:30 +0900

    Refactor: OutputView 수정

--- mission/mission1-calculator/src/main/java/com/calculator/view/OutputView.java
+++ mission/mission1-calculator/src/main/java/com/calculator/view/OutputView.java
@@ -1,9 +1,19 @@
 package com.calculator.view;
 
-import com.calculator.domain.Calculator;
-
 public class OutputView {
-    public int calculationResult(int result){
-        return result;
+    public void printAddResult(int result) {
+        System.out.println("더하기 계산 결과 = " + result);
+    }
+
+    public void printSubtractResult(int result) {
+        System.out.println("빼기 계산 결과 = " + result);
+    }
+
+    public void printMultiplyResult(int result) {
+        System.out.println("곱하기 계산 결과 = " + result);
+    }
+
+    public void printDivideResult(int result) {
+        System.out.println("나누기 계산 결과 = " + result);
     }
 }

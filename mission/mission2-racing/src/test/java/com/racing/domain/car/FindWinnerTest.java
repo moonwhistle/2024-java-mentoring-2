ed9cf0c4b21bd8ecab15a3068141fdb766f6535d
baekseungho <mjbsh02@mju.ac.kr>
2024-12-27 02:06:35 +0900

    Feat: 우승자 구하는 기능 테스트 구현

---
+++ mission/mission2-racing/src/test/java/com/racing/domain/car/FindWinnerTest.java
@@ -0,0 +1,36 @@
+package com.racing.domain.car;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FindWinnerTest {
+
+    @Test
+    void 우승자구하기테스트() {
+
+        //given
+        int maxPosition = 1;
+        int notMaxPosition = 0;
+        Car neo = new Car("neo");
+        Car brie = new Car("brie");
+        neo.driveCar(5);
+        brie.driveCar(2);
+        List<Car> registeredCars = Arrays.asList(neo, brie);
+        Cars recingCars = new Cars(registeredCars);
+        List<String> expected = Arrays.asList(("neo"));
+        List<String> notExpected = Arrays.asList(("brie"));
+
+        //when & then
+        List<String> result = recingCars.findWinner(maxPosition);
+        List<String> notResult = recingCars.findWinner(notMaxPosition);
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(expected, result),
+                () -> Assertions.assertEquals(notExpected, notResult),
+                () -> Assertions.assertNotEquals(expected, notResult),
+                () -> Assertions.assertNotEquals(notExpected, result)
+        );
+    }
+}

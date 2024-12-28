d3d859b04eb40b8edfbaaab79a9c8aafba624fd6
baekseungho <mjbsh02@mju.ac.kr>
2024-12-29 01:52:57 +0900

    Feat: 차명단을 임의대로 만들어 등록하는 메소드와 등록된 차들의 이름과 각 차들의 위치를 반환하는 getter() 메소드 구현

--- mission/mission2-racing/src/main/java/com/racing/domain/car/Cars.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/car/Cars.java
@@ -3,10 +3,12 @@ package com.racing.domain.car;
 import com.racing.domain.number.RandomNumberGenerator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Cars {
 
+    private static final int NUMBER_OF_Cars = 4;
     private static final int NUMBER_OF_RACE_LAPS = 4;
     private static final int NUMBER_ZERO = 0;
 
@@ -16,8 +18,13 @@ public class Cars {
         this.cars = cars;
     }
 
-    public List<Car> getCars() {
-        return cars;
+    public List<Car> makeCars() {
+        List<Car> carEntry = new ArrayList<>();
+        List<String> NamesOfCars = Arrays.asList("a", "b", "c", "d");
+        for (String nameOfCar : NamesOfCars) {
+            carEntry.add(new Car(nameOfCar));
+        }
+        return carEntry;
     }
 
     public void raceCars(RandomNumberGenerator randomNumberGenerator) {
@@ -39,6 +46,26 @@ public class Cars {
                 .mapToInt(v -> v.getPosition())
                 .max()
                 .orElse(0);
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public List<String> getNamesOfCars() {
+        List<String> namesOfCars = new ArrayList<>();
+        for (Car car : cars) {
+            namesOfCars.add(car.getCarName());
+        }
+        return namesOfCars;
+    }
+
+    public List<Integer> getPositionsOfCars() {
+        List<Integer> positionsOfCars = new ArrayList<>();
+        for (Car car : cars) {
+            positionsOfCars.add(car.getPosition());
+        }
+        return positionsOfCars;
     }
 
     private void findRightCar(int maxPosition, Car car, List<String> winners) {

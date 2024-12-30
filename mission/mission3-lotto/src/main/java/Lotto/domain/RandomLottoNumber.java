b1da2cb1fcad800b7945d94e40dd0a1056928cbe
daeyeon <kdy4410121@naver.com>
2024-12-30 19:56:19 +0900

    feat: 입력 받은 개수 만큼 로또 생성하는 함수 생성

--- mission/mission3-lotto/src/main/java/Lotto/domain/RandomLottoNumber.java
+++ mission/mission3-lotto/src/main/java/Lotto/domain/RandomLottoNumber.java
@@ -9,13 +9,22 @@ public class RandomLottoNumber implements RandomNumberGenerator {
 
     private static final int limit = 100;
     private static final int lottoElementNumber = 6;
-    private ArrayList<Integer> lottoList = new ArrayList<>();
+    private ArrayList<Integer> numberList = new ArrayList<>();
+    private ArrayList<ArrayList<Integer>> lottoList = new ArrayList<>();
 
     @Override
     public ArrayList<Integer> generateNumberList(){
         for(int i = 0 ; i < lottoElementNumber; i++){
             int number = random.nextInt(limit);
-            lottoList.add(number);
+            numberList.add(number);
+        }
+
+        return numberList;
+    }
+
+    public ArrayList<ArrayList<Integer>> generateLottoList(int amount){
+        for(int i = 0; i < amount; i++){
+            lottoList.add(generateNumberList());
         }
 
         return lottoList;

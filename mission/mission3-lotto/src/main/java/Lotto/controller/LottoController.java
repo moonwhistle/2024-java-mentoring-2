60f06bdd5965e7ec7d47af86d16c8383ee7d8b7b
daeyeon <kdy4410121@naver.com>
2024-12-30 20:34:43 +0900

    feat: 생성자에 인터페이스 추가

--- mission/mission3-lotto/src/main/java/Lotto/controller/LottoController.java
+++ mission/mission3-lotto/src/main/java/Lotto/controller/LottoController.java
@@ -1,24 +1,25 @@
 package Lotto.controller;
 
+import Lotto.domain.Lotto;
 import Lotto.view.InputView;
 import Lotto.view.OutputView;
+import randomNumber.RandomNumberGenerator;
 
 public class LottoController {
 
     private final InputView inputView;
     private final OutputView outputView;
+    private final RandomNumberGenerator randomNumberGenerator;
 
-    public LottoController(InputView inputView, OutputView outputView){
+    public LottoController(InputView inputView, OutputView outputView, RandomNumberGenerator randomNumberGenerator){
         this.inputView = inputView;
         this.outputView = outputView;
+        this.randomNumberGenerator = randomNumberGenerator;
     }
 
     public void startLotto(){
-        purchaseLotto();
-    }
-
-    private void purchaseLotto(){
-        enterLottoNumber();
+        Lotto lotto = new Lotto(enterLottoNumber(), randomNumberGenerator);
+        int numberOfLotto = enterLottoNumber();
         printLottoNumber();
     }
 

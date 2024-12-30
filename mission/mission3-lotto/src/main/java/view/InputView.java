cb49d3616144ebfad464244b0ede60784b640d97
daeyeon <kdy4410121@naver.com>
2024-12-30 19:15:36 +0900

    feat: 로또 구입 금액 입력 기능 구현

--- mission/mission3-lotto/src/main/java/view/InputView.java
+++ mission/mission3-lotto/src/main/java/view/InputView.java
@@ -5,10 +5,17 @@ import java.util.Scanner;
 public class InputView {
 
     private final Scanner scanner = new Scanner(System.in);
-    private static final int lottoPrice = 1000;
+    private final int lottoPrice = 1000;
+    private final String enterAmount = "구입금액을 입력해 주세요.";
 
-    public int purchaseLotto(int amount){
+    public int purchaseLotto(){
+        printEnterAmount();
+        int amount = scanner.nextInt();
         return amount / lottoPrice;
+    }
+
+    private void printEnterAmount(){
+        System.out.println(enterAmount);
     }
 
 }

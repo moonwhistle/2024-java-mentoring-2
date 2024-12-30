01f1fccfe3c16ad2ef169d0b89dafa2d31575f00
박지후 <105189817+thisisjihoo@users.noreply.github.com>
2024-12-30 15:11:04 +0900

    Refactoring: 코드 가독성 추가

--- mission/mission1-calculator/src/main/java/com/calculator/controller/Calculator.java
+++ mission/mission1-calculator/src/main/java/com/calculator/controller/Calculator.java
@@ -1,18 +1,19 @@
 package com.calculator.controller;
 
 public class Calculator {
-    public static int addCalculator(int a, int b){
-        return a+b;
+    public static int addCalculator(int a, int b) {
+        return a + b;
     }
 
-    public static int subCalculator(int a, int b){
-        return a-b;
+    public static int subCalculator(int a, int b) {
+        return a - b;
     }
 
-    public static int mulCalculator(int a, int b){
-        return a*b;
+    public static int mulCalculator(int a, int b) {
+        return a * b;
     }
-    public static int divCalculator(int a, int b){
-        return a/b;
+
+    public static int divCalculator(int a, int b) {
+        return a / b;
     }
 }

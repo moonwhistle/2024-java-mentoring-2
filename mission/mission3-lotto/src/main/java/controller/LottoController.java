f4d033c2edf57308b34c06750884cd0a5b0811bf
daeyeon <kdy4410121@naver.com>
2024-12-30 19:16:07 +0900

    feat: 로또 구입 개수 출력 함수 생성

--- mission/mission3-lotto/src/main/java/controller/LottoController.java
+++ mission/mission3-lotto/src/main/java/controller/LottoController.java
@@ -1,4 +1,22 @@
 package controller;
 
+import domain.Lotto;
+import view.InputView;
+import view.OutputView;
+
 public class LottoController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public LottoController(InputView inputView, OutputView outputView){
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void printLottoNumber(){
+        int number = inputView.purchaseLotto();
+        System.out.println(number);
+    }
+
 }

6d2dbdcceb1451d9e76b1a04bfd7afb4aab3c37d
baekseungho <mjbsh02@mju.ac.kr>
2024-12-30 16:42:48 +0900

    Style: 상수 네이밍 수정

--- mission/mission2-racing/src/main/java/com/racing/domain/car/Car.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/car/Car.java
@@ -2,19 +2,19 @@ package com.racing.domain.car;
 
 public class Car {
 
-    private static final int INITIAL_POSITION = 0;
-    private static final int MOVEMENT_THRESHOLD = 4;
+    private static final int INITIAL_POSITION_OF_CAR = 0;
+    private static final int THRESHOLD_OF_CAR_MOVEMENT = 4;
 
     private int position;
     private final CarName carName;
 
     public Car(final String carName) {
-        position = INITIAL_POSITION;
+        position = INITIAL_POSITION_OF_CAR;
         this.carName = new CarName(carName);
     }
 
     public void driveCar(int randomNumber) {
-        if (randomNumber >= MOVEMENT_THRESHOLD) {
+        if (randomNumber >= THRESHOLD_OF_CAR_MOVEMENT) {
             forwardCar();
         }
     }

d735d5711231a66bbd01c51dfc914189cede6748
daeyeon <kdy4410121@naver.com>
2024-12-30 21:30:16 +0900

    fix: 로또 리스트에 로또 추가하는 함수 변경

--- mission/mission3-lotto/src/main/java/Lotto/domain/Lotto.java
+++ mission/mission3-lotto/src/main/java/Lotto/domain/Lotto.java
@@ -6,25 +6,19 @@ import java.util.ArrayList;
 
 public class Lotto {
 
-    private final ArrayList<Integer> lottoNumber;
     private final RandomNumberGenerator randomNumberGenerator;
     private final ArrayList<ArrayList<Integer>> lottoList = new ArrayList<>();
 
     public Lotto(int numberOfLotto, RandomNumberGenerator randomNumberGenerator){
         this.randomNumberGenerator = randomNumberGenerator;
-        this.lottoNumber = randomNumberGenerator.generateNumberList();
 
         for(int i = 0; i < numberOfLotto; i++){
-            lottoList.add(lottoNumber);
+            lottoList.add(randomNumberGenerator.generateNumberList());
         }
     }
 
     public ArrayList<ArrayList<Integer>> getLottoList(){
         return lottoList;
-    }
-
-    public ArrayList<Integer> getLottoNumber(){
-        return lottoNumber;
     }
 
 }

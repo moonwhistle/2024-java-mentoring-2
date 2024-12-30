88432b0cb278ed3ec6678b7562a32905ce071843
baekseungho <mjbsh02@mju.ac.kr>
2024-12-30 17:08:05 +0900

    Feat: 레이싱 진행할 랩 수 받는 메소드 추가

--- mission/mission2-racing/src/main/java/com/racing/view/InputView.java
+++ mission/mission2-racing/src/main/java/com/racing/view/InputView.java
@@ -10,4 +10,9 @@ public class InputView {
         System.out.print("차의 이름을 입력하세요");
         return scanner.next();
     }
+
+    public int receiveNumberOfLaps() {
+        System.out.print("레이싱 랩 수를 입력하세요");
+        return scanner.nextInt();
+    }
 }

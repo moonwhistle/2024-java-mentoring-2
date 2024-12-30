21ebd96ced7ed53d3ad1ab41337b5b8c4559c2fb
baekseungho <mjbsh02@mju.ac.kr>
2024-12-30 17:10:03 +0900

    Feat: 차들을 받아 경주를 시키고 우승자를 구하는 레이싱 시스템으로서의 Racing 클래스 생성 및 구현

--- mission/mission2-racing/src/main/java/com/racing/domain/car/Cars.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/car/Cars.java
@@ -1,49 +1,14 @@
 package com.racing.domain.car;
 
-import com.racing.domain.number.RandomNumberGenerator;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Cars {
 
-    private static final int NUMBER_OF_Cars = 4;
-    private static final int NUMBER_OF_RACE_LAPS = 4;
-    private static final int NUMBER_ZERO = 0;
-
     private final List<Car> cars;
 
     public Cars(List<Car> cars) {
         this.cars = cars;
-    }
-
-    public List<Car> makeCarEntry() {
-        List<Car> carEntry = new ArrayList<>();
-        for (int i = 0; i < NUMBER_OF_Cars; i++) {
-            carEntry.add(new Car(makeCars().get(i)));
-        }
-        return carEntry;
-    }
-
-    public void raceCars(RandomNumberGenerator randomNumberGenerator) {
-        for (int i = NUMBER_ZERO; i < NUMBER_OF_RACE_LAPS; i++) {
-            driveCars(randomNumberGenerator);
-        }
-    }
-
-    public List<String> findWinner(int maxPosition) {
-        List<String> winner = new ArrayList<>();
-        for (Car car : cars) {
-            findRightCar(maxPosition, car, winner);
-        }
-        return winner;
-    }
-
-    public int findMaxPosition() {
-        return cars.stream()
-                .mapToInt(v -> v.getPosition())
-                .max()
-                .orElse(0);
     }
 
     public List<Car> getCars() {
@@ -66,23 +31,4 @@ public class Cars {
         return positionsOfCars;
     }
 
-    private List<String> makeCars() {
-        List<String> NamesOfCars = new ArrayList<>();
-        for (char i = 'a'; i < 'a' + NUMBER_OF_Cars; i++) {
-            NamesOfCars.add(i+"");
-        }
-        return NamesOfCars;
-    }
-
-    private void findRightCar(int maxPosition, Car car, List<String> winners) {
-        if (maxPosition == car.getPosition()) {
-            winners.add(car.getCarName());
-        }
-    }
-
-    private void driveCars(RandomNumberGenerator randomNumberGenerator) {
-        for (Car car : cars) {
-            car.driveCar(randomNumberGenerator.generateRandomNumber());
-        }
-    }
 }
---
+++ mission/mission2-racing/src/main/java/com/racing/domain/car/Racing.java
@@ -0,0 +1,45 @@
+package com.racing.domain.car;
+
+import com.racing.domain.number.RandomNumberGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Racing {
+
+    private static final int NUMBER_ZERO = 0;
+
+    public void raceTheCars(RandomNumberGenerator randomNumberGenerator, int numberOfLaps, List<Car> cars) {
+        for (int i = NUMBER_ZERO; i < numberOfLaps; i++) {
+            driveCars(randomNumberGenerator, cars);
+        }
+    }
+
+    public List<String> findWinner(int maxPosition, List<Car> cars) {
+        List<String> winner = new ArrayList<>();
+        for (Car car : cars) {
+            findRightCar(maxPosition, car, winner);
+        }
+        return winner;
+    }
+
+    public int findMaxPosition(List<Car> cars) {
+        return cars.stream()
+                .mapToInt(v -> v.getPosition())
+                .max()
+                .orElse(0);
+    }
+
+
+    private void findRightCar(int maxPosition, Car car, List<String> winners) {
+        if (maxPosition == car.getPosition()) {
+            winners.add(car.getCarName());
+        }
+    }
+
+    private void driveCars(RandomNumberGenerator randomNumberGenerator, List<Car> cars) {
+        for (Car car : cars) {
+            car.driveCar(randomNumberGenerator.generateRandomNumber());
+        }
+    }
+}

d060dbf2b8cc467eba84832d2c11e163194ac917
baekseungho <mjbsh02@mju.ac.kr>
2024-12-30 18:29:56 +0900

    Refactor: 참조 클래스 및 생성자, 레이싱 실행 메소드 수정

--- mission/mission2-racing/src/main/java/com/racing/controller/CarController.java
+++ mission/mission2-racing/src/main/java/com/racing/controller/CarController.java
@@ -2,37 +2,41 @@ package com.racing.controller;
 
 import com.racing.domain.car.Car;
 import com.racing.domain.car.Cars;
+import com.racing.domain.car.Racing;
+import com.racing.domain.car.Registration;
 import com.racing.domain.number.RandomNumberGenerator;
+
 import com.racing.view.InputView;
 import com.racing.view.OutputView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class CarController {
 
+    private final Racing racing;
+    private final Registration registration;
     private final RandomNumberGenerator randomNumberGenerator;
     private final OutputView outputView;
     private final InputView inputview;
 
-    public CarController(RandomNumberGenerator generateRacingRandomNumber, OutputView outputView, InputView inputview) {
+    public CarController(Racing racing, Registration registration, RandomNumberGenerator generateRacingRandomNumber, OutputView outputView, InputView inputview) {
+        this.racing = racing;
+        this.registration = registration;
         this.randomNumberGenerator = generateRacingRandomNumber;
         this.outputView = outputView;
         this.inputview = inputview;
     }
 
     public void run() {
-        Cars cars = new Cars(new ArrayList<>());
-        List<Car> carEntry = cars.makeCarEntry();
-        Cars registeredCars = new Cars(carEntry);
-        List<String> namesOfRacingCars = registeredCars.getNamesOfCars();
-        registeredCars.raceCars(randomNumberGenerator);
-        int maxPosition = registeredCars.findMaxPosition();
-        List<Integer> positionsOfCars = registeredCars.getPositionsOfCars();
-        List<String> winner = registeredCars.findWinner(maxPosition);
-        outputView.showRacingCarsName(namesOfRacingCars);
-        outputView.showCarsPosition(positionsOfCars);
+        String namesOfCars = inputview.receiveCarName();
+        int numberOfLaps = inputview.receiveNumberOfLaps();
+        List<Car> registeredCars = registration.registerCars(namesOfCars);
+        racing.raceTheCars(randomNumberGenerator, numberOfLaps, registeredCars);
+        int maxPositionOfCars = racing.findMaxPosition(registeredCars);
+        List<String> winner = racing.findWinner(maxPositionOfCars, registeredCars);
+        Cars racingCars = new Cars(registeredCars);
+        outputView.showRacingCarsName(racingCars.getNamesOfCars());
+        outputView.showCarsPosition(racingCars.getPositionsOfCars());
         outputView.showWinner(winner);
     }
-
 }

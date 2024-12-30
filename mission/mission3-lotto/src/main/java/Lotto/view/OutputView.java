3e1e1d8b863f3b17b4b7a1c053024b733cd5b833
daeyeon <kdy4410121@naver.com>
2024-12-30 21:30:52 +0900

    feat: 로또 리스트 반환 함수 생성

--- mission/mission3-lotto/src/main/java/Lotto/view/OutputView.java
+++ mission/mission3-lotto/src/main/java/Lotto/view/OutputView.java
@@ -1,11 +1,24 @@
 package Lotto.view;
 
+import Lotto.domain.Lotto;
+
+import java.util.ArrayList;
+
 public class OutputView {
 
     private final static String lottoPurchase = "개를 구매했습니다.";
 
     public void printLottoNumber(int lottoNumber){
+        System.out.println();
         System.out.println(lottoNumber + lottoPurchase);
+    }
+
+    public void printLottoList(Lotto lotto){
+        ArrayList<ArrayList<Integer>> lottoList = lotto.getLottoList();
+
+        for(int i = 0; i < lottoList.size(); i++){
+            System.out.println(lottoList.get(i).toString());
+        }
     }
 
 }

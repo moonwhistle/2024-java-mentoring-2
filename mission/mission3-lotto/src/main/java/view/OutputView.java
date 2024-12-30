a67c95e301bbc1c0127be39abba115b69fa1f289
daeyeon <kdy4410121@naver.com>
2024-12-30 19:22:58 +0900

    feat: 구입한 로또 개수 출력 함수 생성

--- mission/mission3-lotto/src/main/java/view/OutputView.java
+++ mission/mission3-lotto/src/main/java/view/OutputView.java
@@ -2,6 +2,10 @@ package view;
 
 public class OutputView {
 
+    private final static String lottoPurchase = "개를 구매했습니다.";
 
+    public void printLottoNumber(int lottoNumber){
+        System.out.println(lottoNumber + lottoPurchase);
+    }
 
 }

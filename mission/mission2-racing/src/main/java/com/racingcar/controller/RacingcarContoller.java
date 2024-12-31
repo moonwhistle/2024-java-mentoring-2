b7c5d46e2bbca7b33a6e5c755824a124f8c6fac7
맥동우 <t6u80o@naver.com>
2025-01-01 01:03:28 +0900

    controller 완성

--- mission/mission2-racing/src/main/java/com/racingcar/controller/RacingcarContoller.java
+++ mission/mission2-racing/src/main/java/com/racingcar/controller/RacingcarContoller.java
@@ -1,31 +1,22 @@
 package com.racingcar.controller;
 
 import com.racingcar.controller.dto.RequestDTO;
-import com.racingcar.controller.dto.CarDataDTO;
 import com.racingcar.controller.dto.ResponseDTO;
 
-import com.racingcar.domain.Cars;
-import com.racingcar.domain.IntegerParser;
-import com.racingcar.domain.RacingCarLogic;
-
+import com.racingcar.sevice.RaceService;
 import com.racingcar.view.InputView;
 import com.racingcar.view.Outputview;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class RacingcarContoller {
 
     private final InputView inputView;
     private final Outputview outputview;
-    private final RacingCarLogic racingCarLogic;
-    private final IntegerParser integerParser;
+    private final RaceService raceService;
 
-    public RacingcarContoller(){
+    public RacingcarContoller() {
         this.inputView = new InputView();
         this.outputview = new Outputview();
-        this.racingCarLogic = new RacingCarLogic();
-        this.integerParser = new IntegerParser();
+        this.raceService = new RaceService();
     }
 
     public RequestDTO wrapInputViewRequest() {
@@ -38,25 +29,11 @@ public class RacingcarContoller {
         return new RequestDTO(strCarNames, strTryCount);
     }
 
-    public void runCar(){
-
+    public void manageRaceFlow() {
         RequestDTO inputViewRequestDTO = wrapInputViewRequest();
+        ResponseDTO raceResponseDTO = raceService.raceCar(inputViewRequestDTO);
 
-//        RequestDTO inputViewDTO = new RequestDTO(strCarNames, strTryCount);
-//
-//        String[] carArray = racingCarLogic.splitCarName(strCarNames);
-//        ArrayList<String> carNames = racingCarLogic.arrayToArrayList(carArray);
-//
-//        int tryCount = integerParser.parseInteger(strTryCount);
-//
-//        RequestDTO requestDTO = new RequestDTO(carNames, tryCount);
-//
-//        Cars cars = new Cars(carNames);
-//
-//        cars = racingCarLogic.loopTryCount(tryCount, cars);
-//        List<String> winnerList = cars.getWinner();
-//
-//        String winner = racingCarLogic.buildWinnerString(winnerList);
-//        outputview.showWinner(winner);
+        String winners = raceResponseDTO.getWinners();
+        outputview.showWinner(winners);
     }
 }

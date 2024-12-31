6783880c8a6a15903028382d328ef598fbbdfd7a
맥동우 <t6u80o@naver.com>
2025-01-01 01:19:40 +0900

    split string 상수화

--- mission/mission2-racing/src/main/java/com/racingcar/domain/RacingCarLogic.java
+++ mission/mission2-racing/src/main/java/com/racingcar/domain/RacingCarLogic.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class RacingCarLogic {
+    private final String CAR_NAME_SPLIT_PATTERN = ",";
+
     public String[] splitCarName(String carName){
-        return carName.split(",");
+        return carName.split(CAR_NAME_SPLIT_PATTERN);
     }
 
     public Cars loopTryCount(int tryCount, Cars cars){

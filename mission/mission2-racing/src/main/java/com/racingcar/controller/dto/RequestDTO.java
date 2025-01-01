df647abeba109b759ef8c6f9937d959d6e79b1d0
맥동우 <t6u80o@naver.com>
2025-01-01 10:38:34 +0900

    dto record로 변경

--- mission/mission2-racing/src/main/java/com/racingcar/controller/dto/RequestDTO.java
+++ mission/mission2-racing/src/main/java/com/racingcar/controller/dto/RequestDTO.java
@@ -1,19 +1,4 @@
 package com.racingcar.controller.dto;
 
-public class RequestDTO {
-    private final String carNames;
-    private final String tryCount;
-
-    public RequestDTO(String carNames, String tryCount) {
-        this.carNames = carNames;
-        this.tryCount = tryCount;
-    }
-
-    public String getCarNames() {
-        return carNames;
-    }
-
-    public String getTryCount() {
-        return tryCount;
-    }
+public record RequestDTO(String carNames, String tryCount) {
 }
--- mission/mission2-racing/src/main/java/com/racingcar/controller/dto/ResponseDTO.java
+++ mission/mission2-racing/src/main/java/com/racingcar/controller/dto/ResponseDTO.java
@@ -1,21 +1,4 @@
 package com.racingcar.controller.dto;
 
-import java.util.List;
-
-public class ResponseDTO {
-    private final String JOIN_STRING = ", ";
-
-    private final List<String> winnerList;
-
-    public ResponseDTO(List<String> winnerList) {
-        this.winnerList = winnerList;
-    }
-
-    private String getFormattedWinners() {
-        return String.join(JOIN_STRING, this.winnerList);
-    }
-
-    public String getWinners() {
-        return getFormattedWinners();
-    }
+public record ResponseDTO(String winners) {
 }

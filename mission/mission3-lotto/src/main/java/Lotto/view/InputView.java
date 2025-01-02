bba5429413db2588975195de148c94b091ffa200
daeyeon <kdy4410121@naver.com>
2025-01-02 16:58:08 +0900

    feat: 구입 금액 예외 처리

--- mission/mission3-lotto/src/main/java/Lotto/view/InputView.java
+++ mission/mission3-lotto/src/main/java/Lotto/view/InputView.java
@@ -1,17 +1,24 @@
 package Lotto.view;
 
+import Lotto.domain.Lotto;
+
 import java.util.Scanner;
 
 public class InputView {
 
     private final Scanner scanner = new Scanner(System.in);
-    private final int lottoPrice = 1000;
     private final String enterAmount = "구입금액을 입력해 주세요.";
 
     public int purchaseLotto(){
         System.out.println(enterAmount);
-        int amount = scanner.nextInt();
-        return amount / lottoPrice;
+        int inputPrice = scanner.nextInt();
+        validPrice(inputPrice);
+        return inputPrice;
+    }
+
+    private void validPrice(int inputPrice){
+        if(inputPrice < Lotto.lottoPrice)
+            throw new IllegalArgumentException("최소 구입 금액은 1000원입니다.");
     }
 
 }

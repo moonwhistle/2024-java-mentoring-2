9ac6ec1b4ed158cd84039d0bf863c4144fa0b57d
daeyeon <kdy4410121@naver.com>
2025-01-03 17:17:43 +0900

    fix: 코드 구조 변경

--- mission/mission3-lotto/src/main/java/Lotto/domain/Lotto.java
+++ mission/mission3-lotto/src/main/java/Lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
 package Lotto.domain;
 
+import Lotto.exception.ExceptionMessage;
 import randomNumber.RandomNumberGenerator;
 
 import java.util.ArrayList;
@@ -11,21 +12,23 @@ public class Lotto {
     private final RandomNumberGenerator randomNumberGenerator;
     private final List<Integer> lotto;
 
-    public Lotto(RandomNumberGenerator randomNumberGenerator){
+    public Lotto(RandomNumberGenerator randomNumberGenerator) {
         this.randomNumberGenerator = randomNumberGenerator;
 
         lotto = randomNumberGenerator.generateNumberList();
         duplicateNumber();
     }
 
-    private void duplicateNumber(){
-        long lottoNumber = lotto.stream().distinct().count();
-        if(lottoNumber != RandomLottoNumber.lottoElementNumber)
-            throw new IllegalArgumentException("로또 번호에 중복이 있습니다.");
-    }
-
     public List<Integer> getLotto(){
         return new ArrayList<>(lotto);
+    }
+
+    private void duplicateNumber(){
+        long lottoNumber = lotto.stream()
+                .distinct()
+                .count();
+        if(lottoNumber != RandomLottoNumber.lottoElementNumber)
+            throw new IllegalArgumentException(ExceptionMessage.DUPLICATE_NUMBER.getMessage());
     }
 
 }

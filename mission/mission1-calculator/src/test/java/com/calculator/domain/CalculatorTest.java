6883403d9b7c788c389f2c53f6e658f56fb4cde7
박지후 <105189817+thisisjihoo@users.noreply.github.com>
2025-01-04 00:23:11 +0900

    Refactoring: 네이밍 수정 및 와일드 카드 수정

--- mission/mission1-calculator/src/test/java/com/calculator/domain/CalculatorTest.java
+++ mission/mission1-calculator/src/test/java/com/calculator/domain/CalculatorTest.java
@@ -2,17 +2,17 @@ package com.calculator.domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
 
     Calculator calculator = new Calculator();
-    int testNumber1 = 3;
-    int testNumber2 = 5;
+    int testFirstNumber = 3;
+    int testSecondNumber = 5;
     @Test
     void plusTest(){
-        int result = calculator.addCalculator(testNumber1,testNumber2);
-        int testResult = testNumber1 + testNumber2;
+        int result = calculator.addCalculator(testFirstNumber,testSecondNumber);
+        int testResult = testFirstNumber + testSecondNumber;
 
         assertEquals(result, testResult);
 
@@ -20,25 +20,26 @@ class CalculatorTest {
 
     @Test
     void subtractionTest(){
-        int result = calculator.subCalculator(testNumber1,testNumber2);
-        int testResult = testNumber1 - testNumber2;
+        int result = calculator.subCalculator(testFirstNumber,testSecondNumber);
+        int testResult = testFirstNumber - testSecondNumber;
 
         assertEquals(result, testResult);
     }
 
     @Test
     void multiplyTest(){
-        int result = calculator.mulCalculator(testNumber1,testNumber2);
-        int testResult = testNumber1 * testNumber2;
+        int result = calculator.mulCalculator(testFirstNumber,testSecondNumber);
+        int testResult = testFirstNumber * testSecondNumber;
 
         assertEquals(result, testResult);
     }
 
     @Test
     void divideTest(){
-        int result = calculator.divCalculator(testNumber1,testNumber2);
-        int testResult = testNumber1 / testNumber2;
+        int result = calculator.divCalculator(testFirstNumber,testSecondNumber);
+        int testResult = testFirstNumber / testSecondNumber;
 
         assertEquals(result, testResult);
     }
+
 }

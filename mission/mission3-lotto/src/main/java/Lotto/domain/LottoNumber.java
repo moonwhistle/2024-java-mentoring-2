d6b630dedcdb3feb497c726b747fb2fc7a6d8bd4
daeyeon <kdy4410121@naver.com>
2025-01-03 18:09:48 +0900

    feat: 로또 범위 벗어나는 예외 처리

--- mission/mission3-lotto/src/main/java/Lotto/domain/LottoNumber.java
+++ mission/mission3-lotto/src/main/java/Lotto/domain/LottoNumber.java
@@ -1,15 +1,23 @@
 package Lotto.domain;
 
+import Lotto.exception.ExceptionMessage;
+
 public class LottoNumber {
 
     private final int lottoNumber;
 
     public LottoNumber(int lottoNumber){
         this.lottoNumber = lottoNumber;
+        numberOutOfRange();
     }
 
     public int getLottoNumber(){
         return lottoNumber;
+    }
+
+    public void numberOutOfRange(){
+        if(lottoNumber > 45 || lottoNumber < 1)
+            throw new IllegalArgumentException(ExceptionMessage.NUMBER_OUT_OF_RANGE.getMessage());
     }
 
 }
--- mission/mission3-lotto/src/main/java/Lotto/exception/ExceptionMessage.java
+++ mission/mission3-lotto/src/main/java/Lotto/exception/ExceptionMessage.java
@@ -3,7 +3,8 @@ package Lotto.exception;
 public enum ExceptionMessage {
 
     DUPLICATE_NUMBER("duplicateNumber", "로또 숫자에 중복이 있습니다."),
-    INVALID_PRICE("invalidPrice", "로또 구매 금액은 최소 1000원 입니다.");
+    INVALID_PRICE("invalidPrice", "로또 구매 금액은 최소 1000원 입니다."),
+    NUMBER_OUT_OF_RANGE("numberOutOfRange", "로또 숫자 범위를 벗어나는 숫자가 있습니다.");
 
     private final String errorName;
     private final String message;

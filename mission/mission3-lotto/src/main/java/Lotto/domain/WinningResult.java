1c5f077799ade020348f6427fed45806ddba3717
daeyeon <kdy4410121@naver.com>
2025-01-04 19:32:57 +0900

    feat: matchCount enum에 추가

--- mission/mission3-lotto/src/main/java/Lotto/domain/WinningResult.java
+++ mission/mission3-lotto/src/main/java/Lotto/domain/WinningResult.java
@@ -2,19 +2,25 @@ package Lotto.domain;
 
 public enum WinningResult {
 
-    FOURTH_PRICE("fourthPirce", "3개 일치 (5000원)", 0),
-    THIRD_PRICE("thirdPrice", "4개 일치 (50000원)", 0),
-    SECOND_PRICE("secondPrice", "5개 일치 (1500000원)", 0),
-    FIRST_PRICE("firstPrice", "6개 일치 (2000000000원)", 0);
+    FOURTH_PRICE(3, "fourthPirce", "3개 일치 (5000원)", 0),
+    THIRD_PRICE(4, "thirdPrice", "4개 일치 (50000원)", 0),
+    SECOND_PRICE(5, "secondPrice", "5개 일치 (1500000원)", 0),
+    FIRST_PRICE(6, "firstPrice", "6개 일치 (2000000000원)", 0);
 
+    private int matchCount;
     private String prizeName;
     private String prizeMessage;
     private int prizeCount;
 
-    WinningResult(String prizeName, String prizeMessage, int prizeCount){
+    WinningResult(int matchCount, String prizeName, String prizeMessage, int prizeCount){
+        this.matchCount = matchCount;
         this.prizeName = prizeName;
         this.prizeMessage = prizeMessage;
         this.prizeCount =  prizeCount;
+    }
+
+    public int getMatchCount(){
+        return matchCount;
     }
 
     public String getPrizeName(){

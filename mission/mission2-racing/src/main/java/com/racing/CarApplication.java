988094f457d19b0850e51a021c63aa36b3923c3d
baekseungho <mjbsh02@mju.ac.kr>
2025-01-07 17:14:24 +0900

    Refactor: 메인 메서드 추가

--- mission/mission2-racing/src/main/java/com/racing/CarApplication.java
+++ mission/mission2-racing/src/main/java/com/racing/CarApplication.java
@@ -1,3 +1,16 @@
 package com.racing;
 
-public class CarApplication {}
+import com.racing.controller.CarController;
+import com.racing.domain.car.Racing;
+import com.racing.domain.car.Registration;
+import com.racing.domain.number.RandomNumberGenerator;
+import com.racing.view.InputView;
+import com.racing.view.OutputView;
+
+public class CarApplication {
+
+    public static void main(String[] args) {
+        CarController carController = new CarController(new Racing(), new Registration(), new RandomNumberGenerator(), new OutputView(), new InputView());
+        carController.run();
+    }
+}

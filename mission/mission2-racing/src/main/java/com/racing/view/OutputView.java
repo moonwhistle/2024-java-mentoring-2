fcf46ca458246c2fb26cd71e73873083ed773fe0
baekseungho <mjbsh02@mju.ac.kr>
2025-01-07 17:12:03 +0900

    Refactor: 각 랩마다 경주 결과를 출력할 수 있도록 필요없는 메소드 삭제 및 로직 수정

--- mission/mission2-racing/src/main/java/com/racing/view/OutputView.java
+++ mission/mission2-racing/src/main/java/com/racing/view/OutputView.java
@@ -1,19 +1,20 @@
 package com.racing.view;
 
-import java.util.List;
+import com.racing.domain.car.Car;
+import com.racing.domain.car.Cars;
 
 public class OutputView {
 
-    public void showRacingCarsName(List<String> namesOfCars) {
-        System.out.println(namesOfCars);
+    public void showLapRecord(Cars cars) {
+        System.out.println();
+        for(Car car : cars.getCars()) {
+            System.out.println(car.getCarName() + " : " + car.getPosition());
+        }
+        System.out.println();
     }
 
-    public void showCarsPosition(List<Integer> positionsOfCars) {
-        System.out.println(positionsOfCars);
-    }
-
-    public void showWinner(List<String> winner) {
-        System.out.println(winner + "우승");
+    public void showWinner(String winner) {
+        System.out.println(winner + "가 최종 우승했습니다.");
     }
 
 }

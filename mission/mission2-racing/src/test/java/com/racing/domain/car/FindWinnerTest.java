63c919d6708a3591f2960494e4f3859ca9a5262a
baekseungho <mjbsh02@mju.ac.kr>
2025-01-07 17:20:02 +0900

    Style: 테스트 변수, 매개변수 수정

--- mission/mission2-racing/src/test/java/com/racing/domain/car/FindWinnerTest.java
+++ mission/mission2-racing/src/test/java/com/racing/domain/car/FindWinnerTest.java
@@ -12,20 +12,20 @@ public class FindWinnerTest {
     void 우승자구하기테스트() {
 
         //given
-        int maxPosition = 1;
-        int notMaxPosition = 0;
+        int maxPosition = 2;
+        int notMaxPosition = 1;
         Car neo = new Car("neo");
         Car brie = new Car("brie");
         List<Car> registeredCars = Arrays.asList(neo, brie);
         neo.driveCar(5);
         brie.driveCar(2);
-        Racing racingCars = new Racing(registeredCars);
-        List<String> expected = Arrays.asList(("neo"));
-        List<String> notExpected = Arrays.asList(("brie"));
+        Racing racingCars = new Racing();
+        String expected = "neo";
+        String notExpected = "brie";
 
         //when & then
-        List<String> result = racingCars.findWinner(maxPosition);
-        List<String> notResult = racingCars.findWinner(notMaxPosition);
+        String result = racingCars.findWinner(registeredCars,maxPosition);
+        String notResult = racingCars.findWinner(registeredCars,notMaxPosition);
         Assertions.assertAll(
                 () -> Assertions.assertEquals(expected, result),
                 () -> Assertions.assertEquals(notExpected, notResult),

941764ec045a48c96d9208003313457425aeec02
맥동우 <t6u80o@naver.com>
2025-01-10 11:59:40 +0900

    feat : purchaseAmoutDTO 해체하는 메서드 추가

--- mission/mission3-lotto/src/main/java/com/lotto/service/LottoService.java
+++ mission/mission3-lotto/src/main/java/com/lotto/service/LottoService.java
@@ -1,4 +1,11 @@
 package com.lotto.service;
 
+import com.lotto.controller.dto.PurchaseAmountDTO;
+
 public class LottoService {
+
+    public void buildLotto(PurchaseAmountDTO purchaseAmountDTO) {
+        String stringPurchaseAmount = purchaseAmountDTO.purchaseAmount();
+
+    }
 }

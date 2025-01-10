bd09410475dadf254800c1618618dd3c8f2646fe
맥동우 <t6u80o@naver.com>
2025-01-10 23:21:23 +0900

    feat : 랜덤 로또 번호 생성기에서도 정의한 상수 클래스 이용하도록 설정

--- mission/mission3-lotto/src/main/java/com/lotto/domain/LottoNumberGenerator.java
+++ mission/mission3-lotto/src/main/java/com/lotto/domain/LottoNumberGenerator.java
@@ -2,7 +2,13 @@ package com.lotto.domain;
 
 public class LottoNumberGenerator {
 
+    private final LottoConfig lottoConfig;
+
+    public LottoNumberGenerator() {
+        this.lottoConfig = new LottoConfig();
+    }
+
     public int getLottoNumber() {
-        return (int) (Math.random() * 45) + 1;
+        return (int) (Math.random() * lottoConfig.getMaxLottoNumber()) + lottoConfig.getMinLottoNumber();
     }
 }

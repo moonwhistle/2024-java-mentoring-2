400dd0240911b3fa1b15e5d5a62ab4e30c892cc7
맥동우 <t6u80o@naver.com>
2025-01-10 13:35:22 +0900

    feat : 로또 구매 결과 루프하면서 결과 출력하도록 돕는 컨트롤러 메서드 정의

--- mission/mission3-lotto/src/main/java/com/lotto/controller/LottoController.java
+++ mission/mission3-lotto/src/main/java/com/lotto/controller/LottoController.java
@@ -1,20 +1,34 @@
 package com.lotto.controller;
 
 import com.lotto.controller.dto.PurchaseAmountDTO;
+import com.lotto.controller.dto.PurchaseLottoDTO;
+
+import com.lotto.service.LottoService;
 
 import com.lotto.view.InputView;
 import com.lotto.view.OutputView;
 
+import java.util.List;
+import java.util.Set;
+
 public class LottoController {
     private final InputView inputView;
     private final OutputView outputview;
+    private final LottoService lottoService;
 
     public LottoController() {
         this.inputView = new InputView();
         this.outputview = new OutputView();
+        this.lottoService = new LottoService();
     }
 
-    private PurchaseAmountDTO getPurchaseAmountDTO(){
+    public void manageLottoProcess() {
+        PurchaseLottoDTO purchaseLottoDTO = lottoService.buildLotto(getPurchaseAmountDTO());
+
+        showPurchasedLotto(getPurchaseLotto(purchaseLottoDTO));
+    }
+
+    private PurchaseAmountDTO getPurchaseAmountDTO() {
         String purchaseAmount = getPurchaseAmountFromView();
 
         return new PurchaseAmountDTO(purchaseAmount);
@@ -24,5 +38,17 @@ public class LottoController {
         outputview.showPurchaseAmount();
 
         return inputView.getInput();
+    }
+
+    private List<Set<Integer>> getPurchaseLotto(PurchaseLottoDTO purchaseLottoDTO) {
+        return purchaseLottoDTO.purchasedLotto();
+    }
+
+    private void showPurchasedLotto(List<Set<Integer>> purchasedLotto) {
+        outputview.showPurchaseHistory(purchasedLotto.size());
+
+        for(Set<Integer> lotto : purchasedLotto){
+            outputview.showLotto(lotto);
+        }
     }
 }

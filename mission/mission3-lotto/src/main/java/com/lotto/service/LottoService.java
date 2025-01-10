39106a039e7aa2aab48cca69f8880410a7487e71
맥동우 <t6u80o@naver.com>
2025-01-10 12:19:23 +0900

    feat : 주문금액 파싱하는 메서드 서비스에서 사용하기

--- mission/mission3-lotto/src/main/java/com/lotto/service/LottoService.java
+++ mission/mission3-lotto/src/main/java/com/lotto/service/LottoService.java
@@ -2,10 +2,23 @@ package com.lotto.service;
 
 import com.lotto.controller.dto.PurchaseAmountDTO;
 
-public class LottoService {
+import com.lotto.domain.IntegerParser;
+import com.lotto.domain.LottoLogic;
 
-    public void buildLotto(PurchaseAmountDTO purchaseAmountDTO) {
+public class LottoService {
+    private final IntegerParser integerParser;
+
+    public LottoService(){
+        this.integerParser = new IntegerParser();
+    }
+
+    public void buildLotto() {
+
+    }
+
+    private int getPurchaseAmount(PurchaseAmountDTO purchaseAmountDTO){
         String stringPurchaseAmount = purchaseAmountDTO.purchaseAmount();
 
+        return integerParser.parseInteger(stringPurchaseAmount);
     }
 }

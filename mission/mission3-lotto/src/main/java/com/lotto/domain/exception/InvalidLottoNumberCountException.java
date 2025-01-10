5788fdc7fbbca22f1ae7b26a34dff118964ba3bf
맥동우 <t6u80o@naver.com>
2025-01-10 20:08:13 +0900

    feat : 로또 한장의 번호 개수가 6이 아닐때 발생하는 커스텀 에러 정의

---
+++ mission/mission3-lotto/src/main/java/com/lotto/domain/exception/InvalidLottoNumberCountException.java
@@ -0,0 +1,7 @@
+package com.lotto.domain.exception;
+
+public class InvalidLottoNumberCountException extends RuntimeException {
+    public InvalidLottoNumberCountException(String message) {
+        super(message);
+    }
+}

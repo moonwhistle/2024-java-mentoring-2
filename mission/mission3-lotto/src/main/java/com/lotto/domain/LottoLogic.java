c2beb8861d97992ee3ae27eea2b8b4a1574057a2
맥동우 <t6u80o@naver.com>
2025-01-10 13:14:51 +0900

    feat : 6개의 로또 번호를 담은 set 생성 메서드 정의

--- mission/mission3-lotto/src/main/java/com/lotto/domain/LottoLogic.java
+++ mission/mission3-lotto/src/main/java/com/lotto/domain/LottoLogic.java
@@ -1,9 +1,32 @@
 package com.lotto.domain;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class LottoLogic {
     private final int LOTTO_PRICE = 1000;
+    private final int LOTTO_NUM_LIMIT = 6;
 
-    public int getAvailableAmount(int integer){
+    private final LottoNumberGenerator lottoNumberGenerator;
+
+    public LottoLogic(){
+        this.lottoNumberGenerator = new LottoNumberGenerator();
+    }
+
+    public int getAvailableAmount(int integer) {
         return integer / LOTTO_PRICE;
+    }
+
+    public Set<Integer> drawLottoNumbers() {
+        Set<Integer> lottoNumbersSet = new HashSet<>();
+
+        while(lottoNumbersSet.size() != LOTTO_NUM_LIMIT){
+            int lottoNumber = lottoNumberGenerator.getLottoNumber();
+            lottoNumbersSet.add(lottoNumber);
+        }
+
+        return lottoNumbersSet;
     }
 }

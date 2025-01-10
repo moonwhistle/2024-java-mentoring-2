34250c8efb744daa17178560689143f9ca13a487
맥동우 <t6u80o@naver.com>
2025-01-10 13:35:44 +0900

    test : 어플리케이션에서 결과 출력해보기

--- mission/mission3-lotto/src/main/java/com/lotto/LottoApplication.java
+++ mission/mission3-lotto/src/main/java/com/lotto/LottoApplication.java
@@ -1,4 +1,10 @@
 package com.lotto;
 
+import com.lotto.controller.LottoController;
+
 public class LottoApplication {
+    public static void main(String[] args) {
+        LottoController lottoController = new LottoController();
+        lottoController.manageLottoProcess();
+    }
 }

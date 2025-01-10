453ed6228dc85e2d5671df252ec3fbc280cc8c69
맥동우 <t6u80o@naver.com>
2025-01-10 13:25:58 +0900

    fix : 로또 개수 인자로 받아서 출력하기

--- mission/mission3-lotto/src/main/java/com/lotto/view/OutputView.java
+++ mission/mission3-lotto/src/main/java/com/lotto/view/OutputView.java
@@ -5,7 +5,7 @@ public class OutputView {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    public void showPurchaseHistory(){
-        System.out.println("14개를 구매했습니다.");
+    public void showPurchaseHistory(int purchaseAmout){
+        System.out.println(purchaseAmout + "개를 구매했습니다.");
     }
 }

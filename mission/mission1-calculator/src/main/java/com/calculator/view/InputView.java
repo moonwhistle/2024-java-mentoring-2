ee823a32a76c92212b279746802cc39a38ff2101
승엽 <137405141+SEUNGBOONG@users.noreply.github.com>
2025-01-10 17:05:52 +0900

    Update InputView.java

--- mission/mission1-calculator/src/main/java/com/calculator/view/InputView.java
+++ mission/mission1-calculator/src/main/java/com/calculator/view/InputView.java
@@ -4,16 +4,4 @@ import java.util.Scanner;
 
 public class InputView {
     private final Scanner scanner;
-
-    public InputView(Scanner scanner) {
-        this.scanner = scanner;
-    }
-
-    public int firstNumber() {
-        return scanner.nextInt();
-    }
-
-    public int secondNumber() {
-        return scanner.nextInt();
-    }
 }

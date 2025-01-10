7e9ec8f3b3894a071c4d552ad08f1e5fa42c6cb1
맥동우 <t6u80o@naver.com>
2025-01-10 12:30:32 +0900

    feat : 로또 몇개 살 수 있는지 반환하는 메서드 추가

--- mission/mission3-lotto/src/main/java/com/lotto/domain/LottoLogic.java
+++ mission/mission3-lotto/src/main/java/com/lotto/domain/LottoLogic.java
@@ -1,4 +1,9 @@
 package com.lotto.domain;
 
 public class LottoLogic {
+    private final int LOTTO_PRICE = 1000;
+
+    public int getAvailableAmount(int integer){
+        return integer / LOTTO_PRICE;
+    }
 }

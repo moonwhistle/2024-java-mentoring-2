5be135110aa5af2c48d484e31a0162ac41883f57
daeyeon <kdy4410121@naver.com>
2025-01-10 15:19:27 +0900

    fix: service 삭제, 컨트룰러 구조 변경

--- mission/mission3-lotto/src/main/java/Lotto/controller/LottoController.java
+++ mission/mission3-lotto/src/main/java/Lotto/controller/LottoController.java
@@ -2,57 +2,53 @@ package Lotto.controller;
 
 import Lotto.domain.*;
 
-import Lotto.service.LottoService;
 import Lotto.view.InputView;
 import Lotto.view.OutputView;
 import randomNumber.RandomNumberGenerator;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class LottoController {
 
     private final InputView inputView;
     private final OutputView outputView;
     private final RandomNumberGenerator randomNumberGenerator;
-    private final LottoService lottoService;
+    private Lottos lottos;
+    private Lotto lotto;
+    private final Winning winning = new Winning();
 
-    public LottoController(InputView inputView, OutputView outputView, RandomNumberGenerator randomNumberGenerator, LottoService lottoService){
+    public LottoController(InputView inputView, OutputView outputView, RandomNumberGenerator randomNumberGenerator){
         this.inputView = inputView;
         this.outputView = outputView;
         this.randomNumberGenerator = randomNumberGenerator;
-        this.lottoService = lottoService;
     }
 
     public void startLotto(){
-        int numberOfLotto = enterLottoNumber();
-        Lottos lottos = new Lottos(createLottos(numberOfLotto), numberOfLotto);
-        printLottoList(lottos, numberOfLotto);
-        Long matchCount = calculateMatchCount(lottos);
-        matchWinningResult(lottos, matchCount);
+        Lottos lottos = createLottos();
+        printLottoList(lottos);
+        List<LottoNumber> winningNumber = getWinningNumber();
+        Long matchCount = calculateMatchCount(lottos, winningNumber);
+        matchWinningResult(matchCount);
         printWinningResult();
         calculateProfit(lottos, matchCount);
     }
 
-    private List<Lotto> createLottos(int numberOfLotto){
-        return IntStream.range(0, numberOfLotto)
-                .mapToObj(i -> new Lotto(randomNumberGenerator))
-                .collect(Collectors.toList());
+    private int enterLottoNumber(){
+        return inputView.purchaseLotto();
     }
 
-    private int enterLottoNumber(){
-        int inputPrice = inputView.purchaseLotto();
-        return lottoService.calculateLottoNumber(inputPrice);
+    private Lottos createLottos(){
+        int inputPrice = enterLottoNumber();
+        return new Lottos(inputPrice, randomNumberGenerator);
     }
 
     private void printLottoNumber(int lottoNumber){
         outputView.printLottoNumber(lottoNumber);
     }
 
-    private void printLottoList(Lottos lottos, int lottoNumber){
-        printLottoNumber(lottoNumber);
-        String lottoList = lottoService.printLottoList(lottos);
+    private void printLottoList(Lottos lottos){
+        printLottoNumber(lottos.getNumberOfLottos());
+        String lottoList = lottos.printLottoList(lottos);
         outputView.printLottoList(lottoList);
     }
 
@@ -61,30 +57,22 @@ public class LottoController {
         return winningNumber.getWinningNumber();
     }
 
-    private long calculateMatchCount(Lottos lottos){
-        List<LottoNumber> winningNumber = getWinningNumber();
-        return lottoService.calculateWinningResult(lottos, winningNumber);
+    private long calculateMatchCount(Lottos lottos, List<LottoNumber> winningNumber){
+        return lottos.calculateWinningResult(lottos, winningNumber);
     }
 
     private void printWinningResult(){
         outputView.printWinningBar();
-        String winningResult = lottoService.getWinningResult();
+        String winningResult = winning.getWinningResult();
         outputView.printWinningResult(winningResult);
     }
 
-    private void matchWinningResult(Lottos lottos, Long matchCount){
-        if(matchCount == WinningResult.FIRST_PRICE.getMatchCount())
-            WinningResult.FIRST_PRICE.incrementPrizeCount();
-        if(matchCount == WinningResult.SECOND_PRICE.getMatchCount())
-            WinningResult.SECOND_PRICE.incrementPrizeCount();
-        if(matchCount == WinningResult.THIRD_PRICE.getMatchCount())
-            WinningResult.THIRD_PRICE.incrementPrizeCount();
-        if(matchCount == WinningResult.FOURTH_PRICE.getMatchCount())
-            WinningResult.FOURTH_PRICE.incrementPrizeCount();
+    private void matchWinningResult(Long matchCount){
+        winning.matchWinningResult(matchCount);
     }
 
     private void calculateProfit(Lottos lottos, Long matchCount){
-        double profit = lottoService.calculateProfit(lottos, matchCount);
+        double profit = winning.calculateProfit(lottos, matchCount);
         outputView.printProfit(profit);
     }
 

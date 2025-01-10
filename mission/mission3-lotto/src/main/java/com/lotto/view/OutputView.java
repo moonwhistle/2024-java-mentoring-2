f81a563902dd69637af4ed8aae2875837100c9e5
맥동우 <t6u80o@naver.com>
2025-01-10 13:34:52 +0900

    feat : 로또 구매 결과 보여주는 프롬포트 메서드 정의

--- mission/mission3-lotto/src/main/java/com/lotto/view/OutputView.java
+++ mission/mission3-lotto/src/main/java/com/lotto/view/OutputView.java
@@ -1,11 +1,17 @@
 package com.lotto.view;
 
+import java.util.Set;
+
 public class OutputView {
     public void showPurchaseAmount(){
         System.out.println("구입금액을 입력해 주세요.");
     }
 
     public void showPurchaseHistory(int purchaseAmout){
-        System.out.println(purchaseAmout + "개를 구매했습니다.");
+        System.out.println("\n" +purchaseAmout + "개를 구매했습니다.");
+    }
+
+    public void showLotto(Set<Integer> lotto){
+        System.out.println(lotto);
     }
 }

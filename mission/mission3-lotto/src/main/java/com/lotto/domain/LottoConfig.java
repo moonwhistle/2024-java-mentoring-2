8e64c6b37c5e0e1a50dbcef23af795d5185f2229
맥동우 <t6u80o@naver.com>
2025-01-10 23:15:00 +0900

    feat : 로또 상수 따로 클래스로 빼기

---
+++ mission/mission3-lotto/src/main/java/com/lotto/domain/LottoConfig.java
@@ -0,0 +1,24 @@
+package com.lotto.domain;
+
+public class LottoConfig {
+    private static final int LOTTO_PRICE = 1000;
+    private static final int LOTTO_NUMBER_COUNT_LIMIT = 6;
+    private static final int MIN_LOTTO_NUMBER = 0;
+    private static final int MAX_LOTTO_NUMBER = 45;
+
+    public int getLottoPrice() {
+        return LOTTO_PRICE;
+    }
+
+    public int getLottoNumberCountLimit() {
+        return LOTTO_NUMBER_COUNT_LIMIT;
+    }
+
+    public int getMinLottoNumber() {
+        return MIN_LOTTO_NUMBER;
+    }
+
+    public int getMaxLottoNumber() {
+        return MAX_LOTTO_NUMBER;
+    }
+}

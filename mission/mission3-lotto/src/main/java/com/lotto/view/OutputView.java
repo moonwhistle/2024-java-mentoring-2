187f7f4ddfa0c975e8ee9732664fb34b196e1f26
맥동우 <t6u80o@naver.com>
2025-01-10 11:39:42 +0900

    feat : 사용자에게 프롬포트를 띄우기 위해 OutputView.java 클래스와 메서드 추가

---
+++ mission/mission3-lotto/src/main/java/com/lotto/view/OutputView.java
@@ -0,0 +1,11 @@
+package com.lotto.view;
+
+public class OutputView {
+    public void showPurchaseAmountAmount(){
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void showPurchaseHistory(){
+        System.out.println("14개를 구매했습니다.");
+    }
+}

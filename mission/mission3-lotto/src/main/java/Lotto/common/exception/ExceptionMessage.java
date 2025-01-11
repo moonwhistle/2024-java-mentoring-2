af5e47e3cd51a4e8917ce36972a12e46fe8ecfc5
daeyeon <kdy4410121@naver.com>
2025-01-11 16:31:47 +0900

    fix: 예외 메시지 수정

--- mission/mission3-lotto/src/main/java/Lotto/common/exception/ExceptionMessage.java
+++ mission/mission3-lotto/src/main/java/Lotto/common/exception/ExceptionMessage.java
@@ -6,7 +6,7 @@ public enum ExceptionMessage {
     INVALID_LOTTO_NUMBER("invalidLottoNumber", "로또 구매 금액은 최소 1000원 입니다."),
     NUMBER_OUT_OF_RANGE("numberOutOfRange", "로또 숫자 범위를 벗어나는 숫자가 있습니다."),
     ZERO_MATCH_COUNT("zeroMatchCount", "당첨 결과가 없습니다."),
-    INVALID_WINNING_RESULT("invalidWinningResult", "당첨 숫자는 6개이어야 합니다.");
+    INVALID_WINNING_RESULT("invalidWinningResult", "당첨 숫자는 6개이어야 합니다.(중복 숫자가 있는지 확인하세요)");
 
     private final String errorName;
     private final String message;

f3f9b747c06306760e600e85335746c02af5d517
맥동우 <t6u80o@naver.com>
2025-01-11 14:26:44 +0900

    feat : LottoConfig interface로 추상화. 기존 config 클래스는 구현체로 변경

---
+++ mission/mission3-lotto/src/main/java/com/lotto/domain/DefaultLottoConfig.java
@@ -0,0 +1,24 @@
+package com.lotto.domain;
+
+public class DefaultLottoConfig implements LottoConfig {
+    private static final int LOTTO_PRICE = 1000;
+    private static final int LOTTO_NUMBER_COUNT_LIMIT = 6;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+
+    public int getLottoPrice() {
+        return LOTTO_PRICE;
+    }
+
+    public int getLottoTicketNumberCountLimit() {
+        return LOTTO_NUMBER_COUNT_LIMIT;
+    }
+
+    public int getMinLottoNumber() {
+        return MIN_LOTTO_NUMBER;
+    }
+
+    public int getMaxLottoNumber() {
+        return MAX_LOTTO_NUMBER;
+    }
+}
--- mission/mission3-lotto/src/main/java/com/lotto/domain/LottoConfig.java
+++ mission/mission3-lotto/src/main/java/com/lotto/domain/LottoConfig.java
@@ -1,24 +1,8 @@
 package com.lotto.domain;
 
-public class LottoConfig {
-    private static final int LOTTO_PRICE = 1000;
-    private static final int LOTTO_NUMBER_COUNT_LIMIT = 6;
-    private static final int MIN_LOTTO_NUMBER = 1;
-    private static final int MAX_LOTTO_NUMBER = 45;
-
-    public int getLottoPrice() {
-        return LOTTO_PRICE;
-    }
-
-    public int getLottoTicketNumberCountLimit() {
-        return LOTTO_NUMBER_COUNT_LIMIT;
-    }
-
-    public int getMinLottoNumber() {
-        return MIN_LOTTO_NUMBER;
-    }
-
-    public int getMaxLottoNumber() {
-        return MAX_LOTTO_NUMBER;
-    }
+public interface LottoConfig {
+    public int getLottoPrice();
+    public int getLottoTicketNumberCountLimit();
+    public int getMinLottoNumber();
+    public int getMaxLottoNumber();
 }

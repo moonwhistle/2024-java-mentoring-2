e2c869b2b574bc79be1179affb708fca03405be2
ukheeDokey <0514uh@gmail.com>
2025-01-13 21:19:43 +0900

    mission1/step1 제출

--- mission/mission1-calculator/src/main/java/com/calculator/controller/CalculatorController.java
+++ mission/mission1-calculator/src/main/java/com/calculator/controller/CalculatorController.java
@@ -1,5 +1,54 @@
 package com.calculator.controller;
 
+import com.calculator.domain.Calculator;
+import com.calculator.view.InputView;
+import com.calculator.view.OutputView;
+
 public class CalculatorController {
+    private final Calculator calculator;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    private int firstNumber;
+    private int secondNumber;
+
+    public CalculatorController(Calculator calculator, InputView inputView, OutputView outputView) {
+        this.calculator = calculator;
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    private void getNumber(){
+        this.firstNumber = inputView.firstNumber();
+        this.secondNumber = inputView.secondNumber();
+    }
+
+    private void addResult() {
+        int add = calculator.add(firstNumber, secondNumber);
+        outputView.printAddResult(add);
+    }
+
+    private void subtractResult() {
+        int subtract = calculator.subtract(firstNumber, secondNumber);
+        outputView.printSubtractResult(subtract);
+    }
+
+    private void multiplyResult() {
+        int multiply = calculator.multiply(firstNumber, secondNumber);
+        outputView.printMultiplyResult(multiply);
+    }
+
+    private void divideResult() {
+        int divide = calculator.divide(firstNumber, secondNumber);
+        outputView.printDivideResult(divide);
+    }
+
+    public void run() {
+        getNumber();
+        addResult();
+        subtractResult();
+        multiplyResult();
+        divideResult();
+    }
 
 }
--- mission/mission1-calculator/src/main/java/com/calculator/view/InputView.java
+++ mission/mission1-calculator/src/main/java/com/calculator/view/InputView.java
@@ -4,4 +4,16 @@ import java.util.Scanner;
 
 public class InputView {
     private final Scanner scanner;
+
+    public InputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public int firstNumber() {
+        return scanner.nextInt();
+    }
+
+    public int secondNumber() {
+        return scanner.nextInt();
+    }
 }

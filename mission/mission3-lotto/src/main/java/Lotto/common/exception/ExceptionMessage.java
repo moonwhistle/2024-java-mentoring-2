444f5f3f94b10df998747a27bc234f26e6b1e378
daeyeon <kdy4410121@naver.com>
2025-01-14 15:07:10 +0900

    feat: 필요한 예외 처리 추가

--- mission/mission3-lotto/src/main/java/Lotto/common/exception/ExceptionMessage.java
+++ mission/mission3-lotto/src/main/java/Lotto/common/exception/ExceptionMessage.java
@@ -6,7 +6,9 @@ public enum ExceptionMessage {
     INVALID_LOTTO_NUMBER("invalidLottoNumber", "로또 구매 금액은 최소 1000원 입니다."),
     NUMBER_OUT_OF_RANGE("numberOutOfRange", "로또 숫자 범위를 벗어나는 숫자가 있습니다."),
     ZERO_MATCH_COUNT("zeroMatchCount", "당첨 결과가 없습니다."),
-    INVALID_WINNING_RESULT("invalidWinningResult", "당첨 숫자는 6개이어야 합니다.(중복 숫자가 있는지 확인하세요)");
+    INVALID_WINNING_RESULT("invalidWinningResult", "당첨 숫자는 6개이어야 합니다.(중복 숫자가 있는지 확인하세요)"),
+    INVALID_LOTTO_FORMAT("invalidLottoFormat", "로또는 숫자 형태이어야 합니다."),
+    EMPTY_OR_NULL_WINNING_NUMBER("emptyOrNullWinningNumber", "당첨 로또 번호가 비어있습니다 (,를 기준으로 6개 숫자를 입력해야 합니다.");
 
     private final String errorName;
     private final String message;
--- mission/mission3-lotto/src/main/java/Lotto/domain/WinningNumber.java
+++ mission/mission3-lotto/src/main/java/Lotto/domain/WinningNumber.java
@@ -13,18 +13,16 @@ public class WinningNumber {
     private List<LottoNumber> winningNumber;
 
     public WinningNumber(String winningNumber){
+        isNullWinningNumber(winningNumber);
         List<Integer> winning = Arrays.stream(winningNumber.split(splitBy))
                 .map(String::trim)
-                .map(Integer::parseInt)
+                .map(this::parseToInteger)
                 .distinct()
                 .collect(Collectors.toList());
 
-        if(winning.size() != validNumberOfLotto)
-            throw new IllegalArgumentException(ExceptionMessage.INVALID_WINNING_RESULT.getMessage());
+        validateNumberOfLotto(winning);
 
-        this.winningNumber = winning.stream()
-                .map(this::toLottoNumber)
-                .collect(Collectors.toList());
+        parseToLottoNumber(winning);
     }
 
     public List<LottoNumber> getWinningNumber(){
@@ -33,6 +31,32 @@ public class WinningNumber {
 
     private LottoNumber toLottoNumber(int number){
         return new LottoNumber(number);
+    }
+
+    private void parseToLottoNumber(List<Integer> winning){
+        this.winningNumber = winning.stream()
+                .map(this::toLottoNumber)
+                .collect(Collectors.toList());
+    }
+
+    private void validateNumberOfLotto(List<Integer> winning){
+        if(winning.size() != validNumberOfLotto)
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_WINNING_RESULT.getMessage());
+    }
+
+    private Integer parseToInteger(String winning){
+        try{
+            return Integer.parseInt(winning);
+        }
+        catch(NumberFormatException numberFormatException){
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_LOTTO_FORMAT.getMessage());
+        }
+    }
+
+    private void isNullWinningNumber(String winningNumber){
+        if(winningNumber == null || winningNumber.trim().isEmpty()){
+            throw new IllegalArgumentException(ExceptionMessage.EMPTY_OR_NULL_WINNING_NUMBER.getMessage());
+        }
     }
 
 }

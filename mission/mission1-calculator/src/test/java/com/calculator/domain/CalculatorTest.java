2dba30af5e9265d40b2c84439d830e32ec7a6de8
ukheeDokey <0514uh@gmail.com>
2025-01-15 17:27:19 +0900

    Feat : 더하기 테스트 구현

---
+++ mission/mission1-calculator/src/test/java/com/calculator/domain/CalculatorTest.java
@@ -0,0 +1,40 @@
+package com.calculator.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculatorTest {
+    private Calculator calculator;
+    private int firstNumber;
+    private int secondNumber;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+        firstNumber = 7;
+        secondNumber = 3;
+    }
+
+    @Test
+    @DisplayName("더하기 테스트")
+    void addTest() {
+        int expectedResult = calculator.add(firstNumber, secondNumber);
+        int actualResult = firstNumber + secondNumber;
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void subtractTest() {
+    }
+
+    @Test
+    void multiplyTest() {
+    }
+
+    @Test
+    void divideTest() {
+    }
+}

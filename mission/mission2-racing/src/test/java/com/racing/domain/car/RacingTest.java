8844c09c805b90e4f09e45b5c97efd61bbf4f91a
baekseungho <mjbsh02@mju.ac.kr>
2025-01-17 03:16:47 +0900

    Test: 테스트 코드 수정

--- mission/mission2-racing/src/test/java/com/racing/domain/car/RacingTest.java
+++ mission/mission2-racing/src/test/java/com/racing/domain/car/RacingTest.java
@@ -1,35 +1,39 @@
 package com.racing.domain.car;
 
+import com.racing.service.Ranking;
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class FindWinnerTest {
+public class RacingTest {
+
+    Ranking ranking;
+
+    @BeforeEach
+    void setUp() {
+        ranking = new Ranking();
+    }
 
     @Test
     void 우승자구하기테스트() {
 
         //given
-        int maxPosition = 2;
-        int notMaxPosition = 1;
         Car neo = new Car("neo");
         Car brie = new Car("brie");
         List<Car> registeredCars = Arrays.asList(neo, brie);
-        neo.driveCar(5);
-        brie.driveCar(2);
-        Racing racingCars = new Racing();
+        neo.driveCar(9);
+        brie.driveCar(0);
         String expected = "neo";
         String notExpected = "brie";
 
         //when & then
-        String result = racingCars.findWinner(registeredCars,maxPosition);
-        String notResult = racingCars.findWinner(registeredCars,notMaxPosition);
+        String result = ranking.findWinner(registeredCars);
         Assertions.assertAll(
                 () -> Assertions.assertEquals(expected, result),
-                () -> Assertions.assertEquals(notExpected, notResult),
-                () -> Assertions.assertNotEquals(expected, notResult),
                 () -> Assertions.assertNotEquals(notExpected, result)
         );
     }

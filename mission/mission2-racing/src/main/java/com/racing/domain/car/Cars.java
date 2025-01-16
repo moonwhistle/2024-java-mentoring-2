933cb0366942c14eab7cb29201d16a6d51defed7
baekseungho <mjbsh02@mju.ac.kr>
2025-01-17 01:59:12 +0900

    Refactor: Racing 클래스 삭제 및 Cars에게 여러 대의 차를 경주시키는 역할 부여

--- mission/mission2-racing/src/main/java/com/racing/domain/car/Cars.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/car/Cars.java
@@ -1,5 +1,7 @@
 package com.racing.domain.car;
 
+import com.racing.domain.number.RandomNumberGenerator;
+
 import java.util.List;
 
 public class Cars {
@@ -8,6 +10,12 @@ public class Cars {
 
     public Cars(List<Car> cars) {
         this.cars = cars;
+    }
+
+    public void raceTheCars(List<Car> cars, RandomNumberGenerator randomNumberGenerator) {
+        for (Car car : cars) {
+            car.driveCar(randomNumberGenerator.generateRandomNumber());
+        }
     }
 
     public List<Car> getCars() {

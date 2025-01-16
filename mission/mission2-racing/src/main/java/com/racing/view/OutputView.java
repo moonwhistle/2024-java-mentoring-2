5ad6f496113fa20e459e65758bdd43debab03445
baekseungho <mjbsh02@mju.ac.kr>
2025-01-17 01:54:04 +0900

    Refactor: Domain 참조 부분 삭제, 단순 데이터 출력하도록 수정

--- mission/mission2-racing/src/main/java/com/racing/view/OutputView.java
+++ mission/mission2-racing/src/main/java/com/racing/view/OutputView.java
@@ -1,16 +1,9 @@
 package com.racing.view;
 
-import com.racing.domain.car.Car;
-import com.racing.domain.car.Cars;
-
 public class OutputView {
 
-    public void showLapRecord(Cars cars) {
-        System.out.println();
-        for(Car car : cars.getCars()) {
-            System.out.println(car.getCarName() + " : " + car.getPosition());
-        }
-        System.out.println();
+    public void showLapRecord(String carName, String carPosition) {
+        System.out.println(carName + " : " + carPosition);
     }
 
     public void showWinner(String winner) {

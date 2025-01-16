3c24913be2b224f0700ce8a373bf1cfe568f0871
baekseungho <mjbsh02@mju.ac.kr>
2025-01-17 02:44:42 +0900

    Style: 메인 메소드 참조명 수정, 실행 코드 수정

--- mission/mission2-racing/src/main/java/com/racing/CarApplication.java
+++ mission/mission2-racing/src/main/java/com/racing/CarApplication.java
@@ -1,16 +1,19 @@
 package com.racing;
 
-import com.racing.controller.CarController;
-import com.racing.domain.car.Racing;
-import com.racing.domain.car.Registration;
 import com.racing.domain.number.RandomNumberGenerator;
+
+import com.racing.controller.CarController;
+
+import com.racing.service.Ranking;
+import com.racing.service.Registration;
+
 import com.racing.view.InputView;
 import com.racing.view.OutputView;
 
 public class CarApplication {
 
     public static void main(String[] args) {
-        CarController carController = new CarController(new Racing(), new Registration(), new RandomNumberGenerator(), new OutputView(), new InputView());
-        carController.run();
+        CarController carController = new CarController(new Ranking(), new Registration(), new RandomNumberGenerator(), new OutputView(), new InputView());
+        carController.startRacingGame();
     }
 }

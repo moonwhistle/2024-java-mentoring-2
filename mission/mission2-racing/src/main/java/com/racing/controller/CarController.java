bb0a4a43e47cd3a86ef639f318a5d3312627fa2e
baekseungho <mjbsh02@mju.ac.kr>
2025-01-17 02:42:16 +0900

    Refactor: 실행 메소드 내 코드 캡슐화

--- mission/mission2-racing/src/main/java/com/racing/controller/CarController.java
+++ mission/mission2-racing/src/main/java/com/racing/controller/CarController.java
@@ -2,9 +2,13 @@ package com.racing.controller;
 
 import com.racing.domain.car.Car;
 import com.racing.domain.car.Cars;
-import com.racing.domain.car.Racing;
-import com.racing.domain.car.Registration;
 import com.racing.domain.number.RandomNumberGenerator;
+
+import com.racing.dto.RacingRequest;
+import com.racing.dto.RacingResponse;
+
+import com.racing.service.Ranking;
+import com.racing.service.Registration;
 
 import com.racing.view.InputView;
 import com.racing.view.OutputView;
@@ -13,34 +17,56 @@ import java.util.List;
 
 public class CarController {
 
-    private final Racing racing;
+    private final Ranking ranking;
     private final Registration registration;
     private final RandomNumberGenerator randomNumberGenerator;
     private final OutputView outputView;
     private final InputView inputview;
 
-    public CarController(Racing racing, Registration registration, RandomNumberGenerator generateRacingRandomNumber, OutputView outputView, InputView inputview) {
-        this.racing = racing;
+    public CarController(Ranking ranking, Registration registration, RandomNumberGenerator generateRacingRandomNumber, OutputView outputView, InputView inputview) {
+        this.ranking = ranking;
         this.registration = registration;
         this.randomNumberGenerator = generateRacingRandomNumber;
         this.outputView = outputView;
         this.inputview = inputview;
     }
 
-    public void run() {
-        String namesOfCar = inputview.receiveCarName();
+    public void startRacingGame() {
+        RacingRequest racingRequest = showRacingRequest();
+        outputView.showResultComment();
+        showPositionOfCarsForEachLap(racingRequest);
+        RacingResponse racingResponse = showRacingResponse(racingRequest);
+        showRacingResult(racingResponse);
+    }
+
+    private RacingResponse showRacingResponse(RacingRequest racingRequest) {
+        String winner = ranking.findWinner(racingRequest.getNamesOfCar());
+        return new RacingResponse(winner);
+    }
+
+    private RacingRequest showRacingRequest() {
+        String namesOfCars = inputview.receiveCarName();
+        List<Car> cars = registration.registerCars(namesOfCars);
         int numberOfLaps = inputview.receiveNumberOfLaps();
-        List<Car> cars = registration.registerCars(namesOfCar);
-        Cars racingCars = new Cars(cars);
-        System.out.println("실행 결과");
-        System.out.println();
-        outputView.showLapRecord(racingCars);
-        for (int i = 0; i < numberOfLaps; i++) {
-            racing.raceTheCars(cars, randomNumberGenerator);
-            outputView.showLapRecord(racingCars);
+        return new RacingRequest(cars, numberOfLaps);
+    }
+
+    private void showRacingResult(RacingResponse racingResponse) {
+        outputView.showWinner(racingResponse.getWinner());
+    }
+
+    private void showPositionOfCarsForEachLap(RacingRequest racingRequest) {
+        Cars racingCars = new Cars(racingRequest.getNamesOfCar());
+        for (int i = 0; i < racingRequest.getNumberOfLaps(); i++) {
+            showPositionsOfForOneLap(racingRequest.getNamesOfCar());
+            racingCars.raceTheCars(racingRequest.getNamesOfCar(), randomNumberGenerator);
         }
-        int maxPosition = racing.findMaxPosition(cars);
-        String winner = racing.findWinner(cars, maxPosition);
-        outputView.showWinner(winner);
+        showPositionsOfForOneLap(racingRequest.getNamesOfCar());
+    }
+
+    private void showPositionsOfForOneLap(List<Car> cars) {
+        for (Car car : cars) {
+            outputView.showLapRecord(car.getCarName(), car.getPosition());
+        }
     }
 }

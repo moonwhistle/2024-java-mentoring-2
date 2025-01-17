3026774b1521d573a062edbb467a5aada1e004d7
baekseungho <mjbsh02@mju.ac.kr>
2025-01-17 20:58:33 +0900

    Remove:

--- mission/mission2-racing/src/main/java/com/racing/common/exception/EmptyNameValueException.java
+++
@@ -1,5 +0,0 @@
-package com.racing.common.exception;
-
-public class EmptyNameValueException extends RuntimeException {
-    public EmptyNameValueException() { super("차의 이름을 입력하세요.");}
-}
--- mission/mission2-racing/src/main/java/com/racing/common/exception/ExcessiveNameLengthException.java
+++
@@ -1,7 +0,0 @@
-package com.racing.common.exception;
-
-public class ExcessiveNameLengthException extends RuntimeException {
-    public ExcessiveNameLengthException() {
-        super("차의 이름은 5자 이하이어야 합니다.");
-    }
-}

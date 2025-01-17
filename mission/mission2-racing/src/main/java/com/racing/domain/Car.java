2bb7d1b55a6e787b2288f2c8f80c877a785d7046
baekseungho <mjbsh02@mju.ac.kr>
2025-01-17 22:02:50 +0900

    Feat: Car 필드, 생성자 생성

--- mission/mission2-racing/src/main/java/com/racing/domain/Car.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/Car.java
@@ -1,3 +1,11 @@
 package com.racing.domain;
 
-public class Car {}
+public class Car {
+
+    private int carPosition;
+    private final String carName;
+
+    public Car(final String carName) {
+        this.carName = carName;
+    }
+}

dae86f1f5bb563acc5fdb613a5ad419fe2091839
baekseungho <mjbsh02@mju.ac.kr>
2025-01-18 00:50:39 +0900

    Feat: Car 전달받은 수에 따라 차를 운전하는 메서드 추가

--- mission/mission2-racing/src/main/java/com/racing/domain/car/Car.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/car/Car.java
@@ -1,12 +1,20 @@
-package com.racing.domain;
+package com.racing.domain.car;
 
 public class Car {
+
+    private static final int THRESHOLD_TO_MOVE = 4;
 
     private CarPosition carPosition;
     private final CarName carName;
 
     public Car(final CarName carName) {
         this.carName = carName;
+    }
+
+    public void driveCar(int randomNumber) {
+        if(randomNumber >= THRESHOLD_TO_MOVE) {
+            carPosition.addPosition();
+        }
     }
 
     public CarPosition getCarPosition() {
@@ -16,4 +24,6 @@ public class Car {
     public CarName getCarName() {
         return carName;
     }
+
+
 }

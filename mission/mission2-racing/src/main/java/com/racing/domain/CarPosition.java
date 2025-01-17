690ed914f23ec9897fbf4e7adacc8ef8f2c34edc
baekseungho <mjbsh02@mju.ac.kr>
2025-01-17 22:40:48 +0900

    Feat: 음수 위치 예외 발생 검증 로직 추가

--- mission/mission2-racing/src/main/java/com/racing/domain/CarPosition.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/CarPosition.java
@@ -6,10 +6,17 @@ public class CarPosition {
     private int carPosition;
 
     public CarPosition() {
+        validateIsPositivePosition(carPosition);
         this.carPosition = START_POSITION;
     }
 
     public int getCarPosition() {
         return carPosition;
+    }
+
+    private void validateIsPositivePosition(int carPosition) {
+        if (carPosition < START_POSITION) {
+            throw new IllegalArgumentException("음수인 위치는 오류를 발생시킵니다.");
+        }
     }
 }

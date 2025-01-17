a0e26bfa1e26db1b31037718233435ed952a1c5e
baekseungho <mjbsh02@mju.ac.kr>
2025-01-17 23:46:19 +0900

    Feat: 검증 로직 추가

--- mission/mission2-racing/src/main/java/com/racing/domain/number/ZeroToNineNumberGenerator.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/number/ZeroToNineNumberGenerator.java
@@ -12,6 +12,20 @@ public class ZeroToNineNumberGenerator implements RandomNumber {
 
     public int generateRandomNumber() {
         int randomNumber = random.nextInt(9) + 1;
+        validateIsNotNegative(randomNumber);
+        validateIsInRange(randomNumber);
         return randomNumber;
+    }
+
+    private void validateIsNotNegative(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("범위 밖의 음수가 생성되었습니다.");
+        }
+    }
+
+    private void validateIsInRange(int number) {
+        if (number > 9) {
+            throw new IllegalArgumentException("범위 밖의 수가 생성되었습니다.");
+        }
     }
 }

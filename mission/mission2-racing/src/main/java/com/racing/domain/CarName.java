5ba18cc507af72f3bdf9caac9745c0825c40d021
baekseungho <mjbsh02@mju.ac.kr>
2025-01-17 22:23:31 +0900

    Feat: 이름의 길이 검증로직 추가

--- mission/mission2-racing/src/main/java/com/racing/domain/CarName.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/CarName.java
@@ -2,9 +2,12 @@ package com.racing.domain;
 
 public class CarName {
 
+    private static final int CAR_NAME_LENGTH_MAX = 5;
     private final String carName;
 
     public CarName(final String carName) {
+        validateIsNotEmpty(carName);
+        validateNameLength(carName);
         this.carName = carName;
     }
 
@@ -15,6 +18,12 @@ public class CarName {
     private void validateIsNotEmpty(final String carName) {
         if (carName == null || carName.length() == 0) {
             throw new IllegalArgumentException("차의 이름은 빈값이 될 수 없습니다.");
+        }
+    }
+
+    private void validateNameLength(final String carName) {
+        if (carName.length() > CAR_NAME_LENGTH_MAX) {
+            throw new IllegalArgumentException("차 이름은 5자 이하이어야 합니다.");
         }
     }
 }

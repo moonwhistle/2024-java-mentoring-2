5c23b264f0926f476fa7099bb39f08b91dea7ebc
baekseungho <mjbsh02@mju.ac.kr>
2025-01-18 15:49:32 +0900

    Feat: 입력 메서드 추가

--- mission/mission2-racing/src/main/java/com/racing/view/InputView.java
+++ mission/mission2-racing/src/main/java/com/racing/view/InputView.java
@@ -1,5 +1,19 @@
 package com.racing.view;
 
+import java.util.Scanner;
+
 public class InputView {
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    public String receiveCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)를 기준으로 구분).");
+        return scanner.next();
+    }
+
+    public int receiveNumbersOfLap() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return scanner.nextInt();
+    }
 
 }

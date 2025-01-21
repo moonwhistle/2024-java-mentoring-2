2e385c2eecd374bd46fc6f4a99729fb10a9ec36a
baekseungho <mjbsh02@mju.ac.kr>
2025-01-21 16:17:25 +0900

    Feat: 우승자 찾는 메서드 추가

--- mission/mission2-racing/src/main/java/com/racing/domain/car/Ranking.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/car/Ranking.java
@@ -1,9 +1,20 @@
 package com.racing.domain.car;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Ranking {
+
+    private static final String JOINING_SYMBOL = ",";
+
+    public String findWinner(List<Car> cars, int maxPosition) {
+        List<String> winner = new ArrayList<>();
+        for (Car car : cars) {
+            findRightCar(maxPosition, car, winner);
+        }
+        return joinWinner(winner);
+    }
 
     public int findMaxPosition(List<Car> cars) {
         List<String> positions = findAllPositions(cars);
@@ -17,5 +28,15 @@ public class Ranking {
         return cars.stream()
                 .map(Car::getCarPosition)
                 .collect(Collectors.toList());
+    }
+
+    private void findRightCar(int maxPosition, Car car, List<String> winner) {
+        if (maxPosition == car.getCarPosition().length()) {
+            winner.add(car.getCarName());
+        }
+    }
+
+    private String joinWinner(List<String> winner) {
+        return String.join(JOINING_SYMBOL, winner);
     }
 }

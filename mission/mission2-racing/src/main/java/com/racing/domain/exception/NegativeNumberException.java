281e3b20bd2e403844628d5670654475a2a02aca
baekseungho <mjbsh02@mju.ac.kr>
2025-01-21 15:45:52 +0900

    Style: 예외 메세지 수정

--- mission/mission2-racing/src/main/java/com/racing/domain/exception/NegativeNumberException.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/exception/NegativeNumberException.java
@@ -2,6 +2,6 @@ package com.racing.domain.exception;
 
 public class NegativeNumberException extends RuntimeException {
     public NegativeNumberException() {
-        super("숫자나 위치가 음수일 수 없습니다.");
+        super("숫자가 음수일 수 없습니다.");
     }
 }

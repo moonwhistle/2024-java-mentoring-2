0391279666e8ef5a312476147dd6ef321aba94db
baekseungho <mjbsh02@mju.ac.kr>
2025-01-21 16:18:45 +0900

    Feat: 서비스 RacingService 생성, 구현

---
+++ mission/mission2-racing/src/main/java/com/racing/service/RacingService.java
@@ -0,0 +1,37 @@
+package com.racing.service;
+
+import com.racing.domain.car.Car;
+import com.racing.domain.car.Ranking;
+import com.racing.domain.car.Registration;
+
+import com.racing.dto.RacingRequest;
+import com.racing.dto.RacingResponse;
+
+import java.util.List;
+
+public class RacingService {
+
+    private final Registration registration;
+    private final Ranking ranking;
+
+    public RacingService() {
+        this.registration = new Registration();
+        this.ranking = new Ranking();
+    }
+
+    public RacingRequest getRacingRequest(String carNames, int numbersOfLaps) {
+        List<Car> cars = getNamesOfCars(carNames);
+        return new RacingRequest(cars, numbersOfLaps);
+    }
+
+    public RacingResponse getRacingResponse(RacingRequest racingRequest) {
+        int maxPosition = ranking.findMaxPosition(racingRequest.namesOfCars());
+        String winner = ranking.findWinner(racingRequest.namesOfCars(), maxPosition);
+        return new RacingResponse(winner);
+    }
+
+    private List<Car> getNamesOfCars(String carNames) {
+        String[] names = registration.splitNamesOfCars(carNames);
+        return registration.registerCars(names);
+    }
+}

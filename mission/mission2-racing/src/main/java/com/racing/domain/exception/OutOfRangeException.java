88c6e7a0fab48a23240ca46e59e54a89f1aed5b9
baekseungho <mjbsh02@mju.ac.kr>
2025-01-21 15:56:02 +0900

    Style: 예외 메세지 수정

--- mission/mission2-racing/src/main/java/com/racing/domain/exception/OutOfRangeException.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/exception/OutOfRangeException.java
@@ -2,6 +2,6 @@ package com.racing.domain.exception;
 
 public class OutOfRangeException extends RuntimeException {
     public OutOfRangeException() {
-        super("범위를 초과하는 생성되었습니다.");
+        super("범위를 초과하는 수가 생성되었습니다.");
     }
 }

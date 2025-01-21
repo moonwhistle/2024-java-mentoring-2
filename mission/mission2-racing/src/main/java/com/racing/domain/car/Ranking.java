b1091f857729f3f283f6b975f933e2708ac09ca4
baekseungho <mjbsh02@mju.ac.kr>
2025-01-21 16:14:31 +0900

    Feat: 최대 위치 찾는 메서드 추가

--- mission/mission2-racing/src/main/java/com/racing/domain/car/Ranking.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/car/Ranking.java
@@ -1,4 +1,21 @@
 package com.racing.domain.car;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Ranking {
+
+    public int findMaxPosition(List<Car> cars) {
+        List<String> positions = findAllPositions(cars);
+        return positions.stream()
+                .mapToInt(v -> v.length())
+                .max()
+                .orElse(0);
+    }
+
+    private List<String> findAllPositions(List<Car> cars) {
+        return cars.stream()
+                .map(Car::getCarPosition)
+                .collect(Collectors.toList());
+    }
 }

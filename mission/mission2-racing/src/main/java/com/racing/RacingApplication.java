771211be9f00a50575d1175fff809f7948844850
baekseungho <mjbsh02@mju.ac.kr>
2025-01-21 16:22:04 +0900

    Feat: main 메서드 추가

---
+++ mission/mission2-racing/src/main/java/com/racing/RacingApplication.java
@@ -0,0 +1,19 @@
+package com.racing;
+
+import com.racing.controller.RacingController;
+
+import com.racing.domain.number.ZeroToNineNumberGenerator;
+
+import com.racing.service.RacingService;
+
+import com.racing.view.InputView;
+import com.racing.view.OutputView;
+
+public class RacingApplication {
+
+    public static void main(String[] args) {
+        RacingController racingController = new RacingController(new RacingService(),new ZeroToNineNumberGenerator(), new InputView(), new OutputView());
+        racingController.startRacing();
+    }
+}
+

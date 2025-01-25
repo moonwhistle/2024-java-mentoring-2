36bf61c2b45fadfec3e228d05b1ffc15ccdbf2b9
baekseungho <mjbsh02@mju.ac.kr>
2025-01-25 18:02:03 +0900

    Refactor: 이름과 위치를 기록한 Map을 전달받아서 우승자를 찾도록 수정

--- mission/mission2-racing/src/main/java/com/racing/domain/system/Ranking.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/system/Ranking.java
@@ -1,10 +1,22 @@
 package com.racing.domain.system;
 
 import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Ranking {
 
     private static final String JOINING_SYMBOL = ",";
+
+    public List<String> findWinner(List<LinkedHashMap<String, String>> racingRecord) {
+        LinkedHashMap<String, String> lastLap = racingRecord.get(racingRecord.size() - 1);
+        int maxPosition = findMaxPosition(lastLap);
+        return lastLap.entrySet()
+                .stream()
+                .filter(v->v.getValue().length() == maxPosition)
+                .map(Map.Entry::getKey)
+                .toList();
+    }
 
     private int findMaxPosition(LinkedHashMap<String, String> positionRecord) {
         return positionRecord.values()

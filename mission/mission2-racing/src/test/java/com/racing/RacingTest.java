cdd6f1204fc8cbf22dcbb55f0f61e604e55e9375
baekseungho <mjbsh02@mju.ac.kr>
2025-01-26 02:41:28 +0900

    Test: 우승자구하기 테스트 수정

--- mission/mission2-racing/src/test/java/com/racing/RacingTest.java
+++ mission/mission2-racing/src/test/java/com/racing/RacingTest.java
@@ -2,32 +2,29 @@ package com.racing;
 
 import com.racing.domain.car.Car;
 import com.racing.domain.car.CarName;
-import com.racing.domain.car.Ranking;
+import com.racing.domain.system.Ranking;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
-
 public class RacingTest {
 
     @Test
     void 우승자구하기테스트() {
 
         //given
-        CarName neo = new CarName("neo");
-        CarName brie = new CarName("brie");
-        Car neoCar = new Car(neo);
-        Car brieCar = new Car(brie);
-        List<Car> cars = Arrays.asList(neoCar, brieCar);
-        neoCar.moveForwardOrStop(5);
-        brieCar.moveForwardOrStop(0);
+        Ranking ranking = new Ranking();
+        LinkedHashMap<String, String> progressOfLaps = new LinkedHashMap<>();
+        progressOfLaps.put("neo","--");
+        progressOfLaps.put("brie","-");
+        List<LinkedHashMap<String, String>> progressOfRace = List.of(progressOfLaps);
         String expected = "neo";
         String notExpected = "brie";
-        Ranking ranking = new Ranking();
 
         //when & then
-        String result = ranking.findWinner(cars);
+        String result = ranking.findWinner(progressOfRace);
         Assertions.assertAll(
                 () -> Assertions.assertEquals(expected, result),
                 () -> Assertions.assertNotEquals(notExpected, result)

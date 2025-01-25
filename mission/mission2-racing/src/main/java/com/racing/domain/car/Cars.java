cab540fa1f9aa97794ae6b1783aa7d1ef1176f26
baekseungho <mjbsh02@mju.ac.kr>
2025-01-25 18:10:38 +0900

    Feat: Cars 경주상황을 저장하는 메서드 추가

--- mission/mission2-racing/src/main/java/com/racing/domain/car/Cars.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/car/Cars.java
@@ -2,23 +2,40 @@ package com.racing.domain.car;
 
 import com.racing.domain.number.ZeroToNineNumberGenerator;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 public class Cars {
 
     private final List<Car> cars;
+    private final List<LinkedHashMap<String, String>> positionRecords;
 
     public Cars(List<Car> cars) {
         this.cars = cars;
+        this.positionRecords = new ArrayList<>();
     }
 
-    public void forwardOrStopCars(ZeroToNineNumberGenerator randomNumberGenerator) {
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    private void forwardOrStopCars(ZeroToNineNumberGenerator randomNumberGenerator) {
         for(Car car : cars) {
             car.moveForwardOrStop(randomNumberGenerator.generateRandomNumber());
         }
     }
 
-    public List<Car> getCars() {
-        return cars;
+    private void recordPositionsOfCars() {
+        LinkedHashMap<String, String> positions = recordPosition(cars);
+        positionRecords.add(positions);
+    }
+
+    private LinkedHashMap<String, String> recordPosition(List<Car> cars) {
+        LinkedHashMap<String, String> positions = new LinkedHashMap<>();
+        for(Car car : cars) {
+            positions.put(car.getCarName(), car.getCarPosition());
+        }
+        return positions;
     }
 }

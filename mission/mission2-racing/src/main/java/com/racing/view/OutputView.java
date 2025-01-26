3a01193d38906c2cc9bd3d11f72454cb853d988b
baekseungho <mjbsh02@mju.ac.kr>
2025-01-27 02:33:08 +0900

    Feat: OutputView List<LinkedHashMap<>> 순회하며 출력하는 메서드 구현

--- mission/mission2-racing/src/main/java/com/racing/view/OutputView.java
+++ mission/mission2-racing/src/main/java/com/racing/view/OutputView.java
@@ -1,21 +1,32 @@
 package com.racing.view;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+
 public class OutputView {
 
     public void showResultComment() {
         System.out.println("\n실행 결과");
     }
 
-    public void showLapRecord(String carName, String carPosition) {
-        System.out.println(carName + " : " + carPosition);
+    public void showRaceProgress(List<LinkedHashMap<String, String>> raceProgress) {
+        for(LinkedHashMap<String, String> lapProgress : raceProgress) {
+            showLapProgress(lapProgress);
+            makeNewLine();
+        }
     }
 
     public void showWinner(String winner) {
         System.out.println(winner + "가 최종 우승했습니다.");
     }
 
-    public void makeNewLine() {
+    private void makeNewLine() {
         System.out.println();
     }
 
+    private void showLapProgress(LinkedHashMap<String, String> lapProgress) {
+        for(String key : lapProgress.keySet()) {
+            System.out.println(key + ": " + lapProgress.get(key));
+        }
+    }
 }

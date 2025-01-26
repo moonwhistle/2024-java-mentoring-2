6a83c58121580f8dbb0fd1995d383486b9e36af4
baekseungho <mjbsh02@mju.ac.kr>
2025-01-27 02:32:01 +0900

    Style: dto data 네이밍 수정

--- mission/mission2-racing/src/main/java/com/racing/dto/RacingResponse.java
+++ mission/mission2-racing/src/main/java/com/racing/dto/RacingResponse.java
@@ -3,5 +3,5 @@ package com.racing.dto;
 import java.util.LinkedHashMap;
 import java.util.List;
 
-public record RacingResponse(String winner, List<LinkedHashMap<String, String>> racingRecord) {
+public record RacingResponse(String winner, List<LinkedHashMap<String, String>> racingProgress) {
 }

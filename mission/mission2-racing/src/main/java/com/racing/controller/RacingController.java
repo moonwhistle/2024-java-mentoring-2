cca3509943defd00a8bc14319a5b343900a4313b
baekseungho <mjbsh02@mju.ac.kr>
2025-02-03 18:59:11 +0900

    Refactor: Controller 수정

--- mission/mission2-racing/src/main/java/com/racing/controller/RacingController.java
+++ mission/mission2-racing/src/main/java/com/racing/controller/RacingController.java
@@ -8,9 +8,6 @@ import com.racing.service.RacingService;
 import com.racing.view.InputView;
 import com.racing.view.OutputView;
 
-import java.util.LinkedHashMap;
-import java.util.List;
-
 public class RacingController {
 
     private final RacingService racingService;
@@ -27,21 +24,13 @@ public class RacingController {
         RacingRequest racingRequest = createRequest();
         outputView.showResultComment();
         RacingResponse racingResponse = racingService.getRacingResponse(racingRequest);
-        showFullProgress(racingResponse.racingProgress());
-        showWinner(racingResponse.winner());
+        outputView.showRaceProgress(racingResponse.racingProgress());
+        outputView.showWinner(racingResponse.winner());
     }
 
     private RacingRequest createRequest() {
         String namesOfCars = inputView.receiveCars();
         int numberOfLaps = inputView.receiveNumbersOfLap();
         return racingService.getRacingRequest(namesOfCars, numberOfLaps);
-    }
-
-    private void showWinner(String winner) {
-        outputView.showWinner(winner);
-    }
-
-    private void showFullProgress(List<LinkedHashMap<String, String>> raceProgress) {
-        outputView.showRaceProgress(raceProgress);
     }
 }

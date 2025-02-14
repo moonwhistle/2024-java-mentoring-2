91519a5108af4c5c0adcb2ca837f795fff2a0544
baekseungho <mjbsh02@mju.ac.kr>
2025-02-14 17:14:21 +0900

    Feat: LottoApplication 구현

---
+++ mission/mission3-lotto/src/main/java/com/lotto/LottoApplication.java
@@ -0,0 +1,16 @@
+package com.lotto;
+
+import com.lotto.controller.LottoController;
+
+import com.lotto.domain.TicketCounter;
+
+import com.lotto.view.InputView;
+import com.lotto.view.OutputView;
+
+public class LottoApplication {
+
+    public static void main(String[] args) {
+        LottoController lottoController = new LottoController(new InputView(), new OutputView(), new TicketCounter());
+        lottoController.run();
+    }
+}

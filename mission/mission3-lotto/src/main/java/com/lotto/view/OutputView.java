46362efe12355fb82659e5d4189325fea79d3907
baekseungho <mjbsh02@mju.ac.kr>
2025-02-14 17:10:25 +0900

    Feat: OutputView 로또 티켓 출력 구현

--- mission/mission3-lotto/src/main/java/com/lotto/view/OutputView.java
+++ mission/mission3-lotto/src/main/java/com/lotto/view/OutputView.java
@@ -9,4 +9,10 @@ public class OutputView {
     public void showNumberOfTickets(int numberOfTickets) {
         System.out.println(numberOfTickets + "개를 구매했습니다.");
     }
+
+    public void showLottoTickets(List<LottoTicket> lottoTickets) {
+        for(LottoTicket lottoTicket : lottoTickets) {
+            System.out.println(lottoTicket.getLotteryNumbers());
+        }
+    }
 }

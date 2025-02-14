28405445b5ffcf9df7489397b7e21fa991faccb1
baekseungho <mjbsh02@mju.ac.kr>
2025-02-14 17:01:43 +0900

    Feat: LottoNumberGenerator 로또 번호 한자리 추첨 구현

--- mission/mission3-lotto/src/main/java/com/lotto/domain/number/LottoNumberGenerator.java
+++ mission/mission3-lotto/src/main/java/com/lotto/domain/number/LottoNumberGenerator.java
@@ -4,9 +4,17 @@ import java.util.Random;
 
 public class LottoNumberGenerator {
 
+    private static final int FIRST_LOTTERY_NUMBER = 1;
+    private static final int LAST_LOTTERY_NUMBER = 45;
+    private static final int NUMBER_OF_LOTTO_NUMBERS = 6;
+
     private final Random random;
 
     public LottoNumberGenerator() {
         this.random = new Random();
+    }
+
+    private int generateRandomNumber() {
+        return random.nextInt(LAST_LOTTERY_NUMBER) + FIRST_LOTTERY_NUMBER;
     }
 }

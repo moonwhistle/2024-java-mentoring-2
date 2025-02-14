0123554510b3aee6071b674706da219b09121de3
baekseungho <mjbsh02@mju.ac.kr>
2025-02-14 16:49:19 +0900

    Feat: LottoTickets 생성

---
+++ mission/mission3-lotto/src/main/java/com/lotto/domain/Lotto/LottoTickets.java
@@ -0,0 +1,18 @@
+package com.lotto.domain.Lotto;
+
+import com.lotto.domain.number.LottoNumberGenerator;
+
+import java.util.List;
+
+public class LottoTickets {
+
+    private final List<LottoTicket> lottoTickets;
+
+    public LottoTickets(List<LottoTicket> lottoTickets) {
+        this.lottoTickets = lottoTickets;
+    }
+
+    public List<LottoTicket> getLottoTickets() {
+        return lottoTickets;
+    }
+}

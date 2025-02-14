df2307aa3c7d81f879f8e25085bbbaa5c0f2e70e
baekseungho <mjbsh02@mju.ac.kr>
2025-02-14 17:09:23 +0900

    Feat: OutputView 티켓 개수 출력 구현

---
+++ mission/mission3-lotto/src/main/java/com/lotto/view/OutputView.java
@@ -0,0 +1,12 @@
+package com.lotto.view;
+
+import com.lotto.domain.Lotto.LottoTicket;
+
+import java.util.List;
+
+public class OutputView {
+
+    public void showNumberOfTickets(int numberOfTickets) {
+        System.out.println(numberOfTickets + "개를 구매했습니다.");
+    }
+}

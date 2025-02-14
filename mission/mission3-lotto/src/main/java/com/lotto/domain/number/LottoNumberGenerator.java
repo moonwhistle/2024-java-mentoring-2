d9d38bbb090f74fd049890db279209fe3541bfa4
baekseungho <mjbsh02@mju.ac.kr>
2025-02-14 16:57:39 +0900

    Feat: LottoNumberGenerator 생성

---
+++ mission/mission3-lotto/src/main/java/com/lotto/domain/number/LottoNumberGenerator.java
@@ -0,0 +1,12 @@
+package com.lotto.domain.number;
+
+import java.util.Random;
+
+public class LottoNumberGenerator {
+
+    private final Random random;
+
+    public LottoNumberGenerator() {
+        this.random = new Random();
+    }
+}

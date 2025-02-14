8fc448158bee5f8b7454297176c5cfe0c8a0b189
baekseungho <mjbsh02@mju.ac.kr>
2025-02-14 17:14:02 +0900

    Feat: LottoController 실행 메서드

--- mission/mission3-lotto/src/main/java/com/lotto/controller/LottoController.java
+++ mission/mission3-lotto/src/main/java/com/lotto/controller/LottoController.java
@@ -1,8 +1,14 @@
 package com.lotto.controller;
 
+import com.lotto.domain.Lotto.LottoTicket;
+import com.lotto.domain.Lotto.LottoTickets;
 import com.lotto.domain.TicketCounter;
+
 import com.lotto.view.InputView;
 import com.lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoController {
 
@@ -15,5 +21,15 @@ public class LottoController {
         this.inputView = inputView;
         this.outputView = outputView;
         this.counter = counter;
+    }
+
+    public void run() {
+        int money = inputView.receivePriceToBuy();
+        int numberOfTickets = counter.calculateCount(money);
+        outputView.showNumberOfTickets(numberOfTickets);
+        List<LottoTicket> lottoNumbers = new ArrayList<>();
+        LottoTickets lottoTickets = new LottoTickets(lottoNumbers);
+        lottoTickets.createLottoTickets(numberOfTickets);
+        outputView.showLottoTickets(lottoNumbers);
     }
 }

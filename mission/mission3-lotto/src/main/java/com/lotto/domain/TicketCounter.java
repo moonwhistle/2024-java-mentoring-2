4df43b7dfbdf81e533842b7e341e2379b0057ac7
baekseungho <mjbsh02@mju.ac.kr>
2025-02-24 18:41:07 +0900

    Refactor: TicketCounter static 메서드로 유틸리티 메서드 구현

--- mission/mission3-lotto/src/main/java/com/lotto/domain/TicketCounter.java
+++ mission/mission3-lotto/src/main/java/com/lotto/domain/TicketCounter.java
@@ -4,7 +4,7 @@ public class TicketCounter {
 
     private static final int PRICE_OF_LOTTERY = 1000;
 
-    public int calculateCount(int money) {
+    public static int calculateCount(int money) {
         return money / PRICE_OF_LOTTERY;
     }
 }
